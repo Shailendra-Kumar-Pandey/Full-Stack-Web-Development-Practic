ce15f72888ce2d959c776fec9dbe2229c22d6dbd
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-08-05 00:03:48 +0530

    Practic javascript

--- Javascript/day.js
+++ Javascript/day.js
@@ -7,3 +7,6 @@ console.log("hay my name is Shailendra Kumar Pandey...");
 console.log("Name: ",name);
 console.log("Age: ",age);
 console.log("Country: ",country);
+console.log("Country: ",country);
+a = 123;
+console.log("Age: ",age);
