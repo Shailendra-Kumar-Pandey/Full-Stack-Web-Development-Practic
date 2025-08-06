016e8ece33b1d49f9e444696a7f32abdd242e071
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-08-06 11:09:41 +0530

    Practic javasript

---
+++ Javascript/day 2/day2.js
@@ -0,0 +1,14 @@
+let name = "shailendra kumar pandey";
+const age = 29;
+var mobile_no = 9806638963;
+//  console.log("Name -",name);
+//  console.log("Age -",age);
+//  console.log("Mobile_no -",mobile_no);
+console.table([name, age, mobile_no]);
+
+var x = 20;
+const y = 10;
+let z = 10;
+if (true) {
+    
+}
