a56fbf078b4b2828899ca09f416b6b9169fff405
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-08-06 22:31:51 +0530

    Practic javascript

---
+++ Javascript/Variables/data type.js
@@ -0,0 +1,10 @@
+// data types in JavaScript
+
+// 1 primitives datatypes
+
+let age = 30; // number
+console.log(typeof age + " : " + age); // number
+
+
+let name = "John"; // string
+console.log(typeof name +" : "+ name); // string
