12d94a739037dff301e44f70ccd1a16deda1b02c
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-08-06 22:37:59 +0530

    practic javasript

--- Javascript/Variables/data type.js
+++ Javascript/Variables/data type.js
@@ -11,3 +11,16 @@ console.log(typeof name +" : "+ name); // string
 
 let isStudent = true; // boolean
 console.log(typeof isStudent + " : " + isStudent); // boolean
+
+let height = null; // null
+console.log(typeof height + " : " + height); // object (null is a special case in JavaScript)
+
+let weight; // undefined
+console.log(typeof weight + " : " + weight); // undefined
+
+let symbol = Symbol("unique"); // symbol
+console.log(typeof symbol + " : " + symbol); // symbol
+
+let bigIntValue = BigInt(123456789012345678901234567890); // bigint
+console.log(typeof bigIntValue + " : " + bigIntValue); // bigint
+
