fbc9c9b584352b6b1b36af7414fa26b95ba8898c
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-08-07 23:11:38 +0530

    practic javascript

---
+++ Javascript/Variables/type conversion.js
@@ -0,0 +1,12 @@
+// Implisit type conversion
+
+console.log("5" + 5); // "55" - string concatenation
+console.log("5" - 5); // 0 - string to number conversion
+console.log("5" * 5); // 25 - string to number conversion
+console.log("5" / 5); // 1 - string to number conversion
+// Output: true - both refer to the same array object
+
+
+
+// explicit type conversion
+
