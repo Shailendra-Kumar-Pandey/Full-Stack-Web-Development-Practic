547c14cea333d2ec7b11aa861e04481eb3ea8afa
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-08-08 23:11:30 +0530

    Practic javascript

---
+++ Javascript/If else if/3 out of Biggest Number.js
@@ -0,0 +1,11 @@
+const a = 30;
+const b = 20;
+const c = 50;
+
+if (a > b && a > c) {
+    console.log("A is Biggest");
+}else if(B > c){
+    console.log("B is Biggest");
+}else{
+    console.log("C is Biggest");
+}
