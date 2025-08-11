c02b2e21d704ff144acc756de5b382524c2e7e5a
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-08-11 22:37:49 +0530

    Add folder and js file

---
+++ Javascript/Switch Statment/enter number find week day.js
@@ -0,0 +1,12 @@
+/* write a program to find the week day */
+
+const day = 3;
+switch (day) {
+    case 1:
+        console.log("Monday");
+        break;
+        
+
+    default:
+        break;
+} 
