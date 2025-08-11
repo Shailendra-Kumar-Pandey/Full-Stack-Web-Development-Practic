c8327a1ce1a478100803d787fa5938c5b24c7063
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-08-11 23:10:54 +0530

    Practic js

---
+++ Javascript/Loops/For/print even and odd number.js
@@ -0,0 +1,10 @@
+/* Write a program to 1 to 20 but print even and odd numbers separately */
+
+for (let i = 1; i <= 20; i++) {
+    if (i % 2 === 0) {
+        console.log(i + " is even");
+    } else {
+        console.log(i + " is odd");
+    }
+}
+
