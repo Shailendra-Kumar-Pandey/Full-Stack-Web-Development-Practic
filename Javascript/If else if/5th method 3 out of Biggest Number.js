ddbc8d58eea1a1f79886f07fde493abe5e594533
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-08-11 22:24:56 +0530

    Practic js

--- Javascript/If else if/5th method 3 out of Biggest Number.js
+++ Javascript/If else if/5th method 3 out of Biggest Number.js
@@ -1,1 +1,12 @@
 /* Write a Program to find of 3 Number out of Biggest  */
+
+const a = 40;
+const b = 20;
+const c = 30;
+
+let big;
+
+big = (a>b) ? a : b;
+big = (big>c) ? big : c;
+
+console.log("The biggest number is: ",  big);
