41424214b808dc483ff0a039399d9f57326c104d
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-08-11 22:14:43 +0530

    Practic javasript

--- Javascript/If else if/3rd Method 3 out of Biggest.js
+++ Javascript/If else if/3rd Method 3 out of Biggest.js
@@ -1,1 +1,19 @@
 /* Write a program to find and print 3 Number out of Biggest  */
+
+const a = 130;
+const b = 70;
+const c = 90;
+
+if (a > b){
+    if(a > c){
+        console.log(a);
+    }else{
+        console.log(c);
+    }
+}else{
+    if(b > c){
+        console.log(b);
+    }else{
+        console.log(c);
+    }
+}
