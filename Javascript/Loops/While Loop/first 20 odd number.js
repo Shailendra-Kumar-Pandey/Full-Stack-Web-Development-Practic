85cf10922f041faabc601c3446af7ca872c6a37f
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-08-11 23:24:57 +0530

    Practic js

--- Javascript/Loops/While Loop/first 20 odd number.js
+++ Javascript/Loops/While Loop/first 20 odd number.js
@@ -1,2 +1,12 @@
 /* Write a program to genrate and print the first 20 odd numbers */
 
+let i = 1;
+let count = 0;
+
+while (count < 20) {
+    if(i%2 != 0){
+        console.log(i);
+        count++;
+    }
+    i++;
+}
