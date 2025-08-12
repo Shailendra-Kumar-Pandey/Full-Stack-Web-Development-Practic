819cfc4babd2de68c09a891e24f9bf7b07410278
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-08-12 17:55:19 +0530

    Practic javascript

--- Javascript/Loops/While Loop/First Format/ganrate odd 15 number.js
+++ Javascript/Loops/While Loop/First Format/ganrate odd 15 number.js
@@ -1,9 +1,20 @@
 /* Write a Program to generate and print the first 15 odd numbers */
 
-let i = -1;
+// let i = -1;
 
-while (i< 15)
-{ 
-    i += 2
-    console.log(i);
+// while (i< 15)
+// { 
+//     i += 2
+//     console.log(i);
+// }
+
+let count = 0;
+let i = 0;
+while (count < 15) {
+    i++;
+    if(i%2 === 1)
+    {
+        console.log(i);
+        count++;
+    }
 }
