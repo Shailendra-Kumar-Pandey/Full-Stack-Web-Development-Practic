5656e752fce0c6d51e665cede8cd2949ab2c35ae
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-08-12 20:39:20 +0530

    Practic javascript

--- Javascript/Object/Practic Object.js
+++ Javascript/Object/Practic Object.js
@@ -1,2 +1,18 @@
 // Object
 
+/*
+let student ={
+    key : value,
+}
+*/
+
+// Practicing Objects in JavaScript
+
+let student = {
+    firstName : "Shailendra",
+    lastName : "Pandey",
+    age : 30,
+    city : "Satna"
+}
+
+console.log(student);
