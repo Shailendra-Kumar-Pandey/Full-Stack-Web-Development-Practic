ff7ee9a6fac3f85f3878826d683f0f87670199c2
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-08-12 15:24:22 +0530

    Practic js

--- Javascript/Loops/Do...While Loop/7 out of Biggest number.js
+++ Javascript/Loops/Do...While Loop/7 out of Biggest number.js
@@ -1,2 +1,33 @@
 /* Write a program to print and find 7 number out of find Biggest Number */
 
+const num1 = 33;
+const num2 = 44;
+const num3 = 11;
+const num4 = 77;
+const num5 = 99;
+const num6 = 88;
+const num7 = 14;
+
+let i = 1;
+let biggest = num1;
+
+console.log("Numbers:");
+do {
+    let currentNum;
+    switch(i) {
+        case 1: currentNum = num1; break;
+        case 2: currentNum = num2; break;
+        case 3: currentNum = num3; break;
+        case 4: currentNum = num4; break;
+        case 5: currentNum = num5; break;
+        case 6: currentNum = num6; break;
+        case 7: currentNum = num7; break;
+    }
+    console.log(currentNum);
+    if (currentNum > biggest) {
+        biggest = currentNum;
+    }
+    i++;
+} while (i <= 7);
+
+console.log("Biggest Number is:", biggest);
