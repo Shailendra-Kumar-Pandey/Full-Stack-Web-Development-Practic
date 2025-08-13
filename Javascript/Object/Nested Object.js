a176214d01db3044d0181174ae2dd00341d42026
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-08-13 23:26:17 +0530

    Practic javascript

--- Javascript/Object/Nested Object.js
+++ Javascript/Object/Nested Object.js
@@ -1,5 +1,29 @@
 //  Nested Objects
 
 const Student = {
-    name
+    firstName : "Shailendra",
+    lastName : "Pandey",
+    age : 29,
+    address : {
+        pinCode : 485001,
+        city : "Satna",
+        state : "Madhya Pradesh"
+    },
+    mobile : 9806638963    
 }
+
+console.log(Student);       //  Print full Object 
+
+console.log(Student.address);   // Print nested object
+
+console.log(Student.address.pinCode);   // 485001
+
+console.log(Student.firstName);     // Shailendra
+
+console.log(Student.address.state);     // Madhya Pradesh
+
+console.log(typeof Student.mobile);    // type of -  Number
+
+console.log(typeof Student.address);    // type of - object
+
+console.log(Student.mobile);    // 9806638963
