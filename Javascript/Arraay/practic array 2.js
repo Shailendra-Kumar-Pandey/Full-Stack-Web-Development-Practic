4ce2347411d851524df336669655482f38780795
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-08-14 00:11:55 +0530

    Practic javascript

--- Javascript/Arraay/practic array 2.js
+++ Javascript/Arraay/practic array 2.js
@@ -1,2 +1,22 @@
 // Practic Array
 
+const student = [
+    {name : "Shailendra", age : 29, city : "Satna"},
+    {name : "Gayatri", age : 26, city : "Satna"},
+    {name : "Aarti", age : 25, city : "Nagpur"},
+    {name : "Navam", age : 12, city : "Satna"},
+]
+
+console.log(student);   // Print student array
+
+console.log(student[0]); // Print first student object
+
+console.log(student[0].name); // Print name of first student    
+
+// Condition is Age >= 18  && city = Satna
+
+let newResultArr = [];
+
+for (let i = 0; i < students.length; i ++){
+    if()
+}
