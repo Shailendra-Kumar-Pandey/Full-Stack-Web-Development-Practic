12e28cf4f6d6972bd8a47e4e7d6b2f70c21ebff8
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-08-14 13:42:04 +0530

    practic javascript

--- Javascript/Arraay/Nested object.js
+++ Javascript/Arraay/Nested object.js
@@ -1,2 +1,9 @@
 //   Nested multiple objects
 
+const data = [
+    { name: "Shailendar Kumar Pandey", age: 30, location: "India", address: {pinCode: 485001, city: "Satna", state: "Madhya Pradesh"} },
+    { name : "Gayatri Shailendra Pandey", age: 27, location: "India"}
+]
+
+
+console.log(data); 
