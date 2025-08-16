f42e388c36731056fb76aac97a2b9676a606b273
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-08-16 22:55:32 +0530

    Practic javaScript

--- Javascript/Function/multiple statement.js
+++ Javascript/Function/multiple statement.js
@@ -1,2 +1,17 @@
 //      Multiple statements
 
+function multipleStatements(num){
+    if(num % 2 === 0){
+        console.log(1);
+        return "Even Number";
+    }
+    console.log(2);
+    if(num % 2 !== 0){
+        console.log(3);
+        return "Odd Number";
+    }
+}
+
+multipleStatements(4);          // print 1
+
+console.log(multipleStatements(4));          // print 1 and Even Number
