7dc0acd047f55baf140c5b08439a27cbb680a953
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-08-17 00:52:04 +0530

    Practic javascript

--- Javascript/Function/multiple statement.js
+++ Javascript/Function/multiple statement.js
@@ -19,4 +19,8 @@ console.log(multipleStatements(4));          // print 1 and Even Number
 console.log(multipleStatements(7));          // print 2; 3 and Odd Number
 
 let result = multipleStatements(7);
+
 console.log(result);          // print 2; 3 and Odd Number
+
+
+
