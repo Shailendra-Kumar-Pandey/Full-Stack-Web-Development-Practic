d9b43f66f26d26aaa8b14d16e224de353c905958
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-08-20 16:46:18 +0530

    Practic Javascript

--- Javascript/Function/Types of Function.js
+++ Javascript/Function/Types of Function.js
@@ -7,3 +7,12 @@ function someThing() {
     // code
 }
 
+// Return: The return keyword sends back a value from the function
+
+// 2. Function Expression
+
+const someFunction = function() {
+    console.log("This is a function expression");
+}
+
+someFunction(); // Invoking the function
