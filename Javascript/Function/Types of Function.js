e869e8e85c140126dd05a2ed26600e8b854cf28f
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-08-20 17:13:19 +0530

    practic javascript

--- Javascript/Function/Types of Function.js
+++ Javascript/Function/Types of Function.js
@@ -9,10 +9,24 @@ function someThing() {
 
 // Return: The return keyword sends back a value from the function
 
+
+addFunction(5, 10); // Invoking the function        // output - 15      // using hosting
+function addFunction(a, b){
+    console.log(a + b);
+}
+addFunction(15, 20);        // 15
+
 // 2. Function Expression
+
+
+/*  using const
+// someFunction();         // before call the function define // Invoking the function  // Error
+//console.log(someFunction); // Logging the function itself return undefined    // error not initialized 
+*/
 
 const someFunction = function() {
     console.log("This is a function expression");
 }
 
 someFunction(); // Invoking the function
+console.log(someFunction()); // Logging the function itself return undifine
