8a863ab58eecf90475c9bbb3a98451c2e7375be9
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-08-20 22:05:07 +0530

    Practic javascript

--- Javascript/Switch Statement/Chack weeks day.js
+++ Javascript/Switch Statement/Chack weeks day.js
@@ -1,2 +1,29 @@
 /*Write a program to check the week day*/
 
+let day = 5;
+
+switch(day){
+    case 1 :
+        console.log("Today is Manday...");
+    break;
+    case 2 :
+        console.log("Today is Tuesday...");
+    break;
+    case 3 :
+        console.log("Today is Wednesday...");
+    break;
+    case 4 :
+        console.log("Today is Thursday...");
+    break;
+    case 5 :
+        console.log("Today is Friday...");
+    break;
+    case 6 :
+        console.log("Today is Saturday...");
+    break;
+    case 7 :
+        console.log("Today is Sunday...");
+    break;
+    default :
+        console.log("Invalid day...");
+}
