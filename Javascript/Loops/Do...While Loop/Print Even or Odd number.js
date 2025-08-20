7a39ac4455401452d0cf28f9232111462a65c248
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-08-21 00:15:21 +0530

    Practic java script

--- Javascript/Loops/Do...While Loop/Print Even or Odd number.js
+++ Javascript/Loops/Do...While Loop/Print Even or Odd number.js
@@ -1,1 +1,13 @@
 /* Write a program to print Even and Odd numbers */
+
+
+let i = 1; // Initialize the counter variable
+
+do {
+    if (i % 2 == 0) {
+        console.log(i + " Even Number");
+    } else {
+        console.log(i + " Odd Number");
+    }
+    i++; // Increment the counter variable
+} while (i <= 20);
