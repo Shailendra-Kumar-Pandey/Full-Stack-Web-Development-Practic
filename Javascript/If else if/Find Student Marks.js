c3d6797398401ccb80951f86f924b0ef6a527893
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-08-20 21:37:39 +0530

    PRactic javascript

--- Javascript/If else if/Find Student Marks.js
+++ Javascript/If else if/Find Student Marks.js
@@ -5,3 +5,14 @@
 Above 80 : Grade A
 */
 
+let marks = 99;
+
+if(marks >= 0 && marks <=34){
+    console.log("Bro..You are Fail...");
+}else if(marks >= 35 && marks <= 60){
+    console.log("Grade - C")
+}else if(marks >= 61 && marks <= 80){
+    console.log("Grade - B")
+}else{
+    console.log("Grade - A")
+}
