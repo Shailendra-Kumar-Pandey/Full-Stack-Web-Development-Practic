16d4bbe2e1bb1a7ec8458c940e7bba003cd91178
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-08-21 22:41:40 +0530

    Practic javascript

--- Javascript/Loops/For Loop/10 prime Number.js
+++ Javascript/Loops/For Loop/10 prime Number.js
@@ -1,14 +1,18 @@
 /* Write a program to print the first 10 prime numbers  */
 
 
- let j=2,i=2;
-    for(i=2;i<70;i++)
+ let j=2, i=2, count=0;
+    for(i=2;i<100;i++)
     {
         for(j=2;j<i;j++)
         {
             if(i%j==0)
                 break;
         }
-        if(i==j)
+        if(i==j){
             console.log(i);
+            count++;
+            if(count==20)
+                break;
+        }
     }
