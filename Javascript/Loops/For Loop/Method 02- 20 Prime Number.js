d654a0b3f71d19e5306b7b525223c31417c5076c
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-08-21 23:34:45 +0530

    Practic js

--- Javascript/Loops/For Loop/Method 02- 20 Prime Number.js
+++ Javascript/Loops/For Loop/Method 02- 20 Prime Number.js
@@ -1,2 +1,21 @@
 /* Write a program to print the first 20 prime numbers */
 
+let count = 0;    // कितने prime मिल चुके हैं
+let number = 2;   // 2 से शुरू करेंगे
+
+for (; count < 20; number++) {      // जब तक 20 prime नहीं मिल जाते, number बढ़ाते रहेंगे
+    let isPrime = true;         // मान लेते हैं कि number prime है
+
+    // दूसरा for loop: check करना कि number prime है या नहीं
+    for (let i = 2; i < number; i++) {          // 2 से लेकर number-1 तक
+        if (number % i === 0) {     // अगर number को i से भाग देने पर remainder 0 आता है
+            isPrime = false;        // तो number prime नहीं है
+            break;              // loop से बाहर निकल जाओ
+        }
+    }
+
+    if (isPrime) {          // अगर number prime है
+        console.log(number); // prime print करो
+        count++;             // prime मिल गया
+    }
+}
