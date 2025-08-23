7043af1f64368be535a3ce4ba4576104ce6eb7e1
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-08-23 22:48:56 +0530

    Practic Javascript

--- Javascript/Object/P-02 Object.js
+++ Javascript/Object/P-02 Object.js
@@ -1,1 +1,12 @@
 //Practic Object
+
+const bankCustmarData = {
+    accountNumber : 39478969921,
+    accountHolder : "Shailendra Kumar Pandey",
+    mobileNumber : 9806638963
+}
+
+console.log(bankCustmarData);       // Print full Object
+
+console.log(bankCustmarData.accountHolder);     // Shailendra Kumar Pandey
+
