f3d2a7386542edd43d2ccdee64c1443a1b6d4aaa
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-08-23 22:57:42 +0530

    Practic Javascript

--- Javascript/Object/P-02 Object.js
+++ Javascript/Object/P-02 Object.js
@@ -10,3 +10,6 @@ console.log(bankCustmarData);       // Print full Object
 
 console.log(bankCustmarData.accountHolder);     // Shailendra Kumar Pandey
 
+bankCustmarData.Address = "Maa Sharda Bhawan in front of Samritan Hospital IP Nagar Pateri Satna (Madhya Pradesh)";     // Add new element key and Value
+
+console.log(bankCustmarData)
