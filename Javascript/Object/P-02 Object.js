3b8aabb00b63d4ba91e9427a522d7ed52640f930
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-08-23 23:18:29 +0530

    Practic Javascript

--- Javascript/Object/P-02 Object.js
+++ Javascript/Object/P-02 Object.js
@@ -1,9 +1,10 @@
-//Practic Object
+//Practic in  Object
 
 const bankCustmarData = {
     accountNumber : 39478969921,
     accountHolder : "Shailendra Kumar Pandey",
-    mobileNumber : 9806638963
+    mobileNumber : 9806638963,
+    custmarId : undefined
 }
 
 console.log(bankCustmarData);       // Print full Object
@@ -12,4 +13,28 @@ console.log(bankCustmarData.accountHolder);     // Shailendra Kumar Pandey
 
 bankCustmarData.Address = "Maa Sharda Bhawan in front of Samritan Hospital IP Nagar Pateri Satna (Madhya Pradesh)";     // Add new element key and Value
 
-console.log(bankCustmarData)
+bankCustmarData.balence = 100000;       // Add new element Property and Value
+
+console.log(bankCustmarData.balence);       //  100000
+
+console.log(bankCustmarData);        // Print full Object
+
+let property = "locker in holder";
+
+let value = "Dimand, Gold, Silver, Cash";
+
+bankCustmarData[property] = value;          // add new element property and value
+
+console.log(bankCustmarData);           // Print full Object
+
+property = "pinCode";
+
+bankCustmarData[property] = ""
+
+console.log(bankCustmarData);
+
+bankCustmarData.custmarId = 1234567890
+
+console.log(bankCustmarData);
+
+
