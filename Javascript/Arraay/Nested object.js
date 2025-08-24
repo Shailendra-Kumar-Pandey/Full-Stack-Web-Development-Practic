0ca0a05d7581e1df18c0e52b022ee0dff1f3947b
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-08-24 22:12:03 +0530

    js practic

--- Javascript/Arraay/Nested object.js
+++ Javascript/Arraay/Nested object.js
@@ -2,7 +2,7 @@
 
 const data = [
     { name: "Shailendar Kumar Pandey", age: 30, location: "India", address: {pinCode: 485001, city: "Satna", state: "Madhya Pradesh"} },
-    { name : "Gayatri Shailendra Pandey", age: 27, location: "India", data1 : [fatherName= "Gomti Prashad Sharma",age= 48, address = "Saleha, Panna (M.P)"]}
+    { name : "Gayatri Shailendra Pandey", age: 27, location: "India", data1 : ["Gomti Prashad Sharma", 48, "Saleha, Panna (M.P)"]}
 ]
 
 
