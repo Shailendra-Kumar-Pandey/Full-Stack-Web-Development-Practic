c4fd9257442af9d338b1ba821d566618e8337adf
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-08-24 23:51:37 +0530

    Practic Javascript

--- Javascript/Function/1 to 10 number print.js
+++ Javascript/Function/1 to 10 number print.js
@@ -1,1 +1,13 @@
 /* Write a program to print numbers from 1 to 10 print  */
+
+let num = 0;
+
+function printSomething(){
+    if(num<10){
+        num++;
+        console.log(num);
+        printSomething();
+    }
+}
+
+printSomething();
