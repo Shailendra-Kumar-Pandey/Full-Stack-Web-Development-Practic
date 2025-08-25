bae29404a296cb9d2557c65d774427e0fe158573
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-08-25 22:35:18 +0530

    Practic javascript

--- Javascript/String Prototype Function/1st to 5th Prototype.js
+++ Javascript/String Prototype Function/1st to 5th Prototype.js
@@ -1,2 +1,27 @@
 //      Prototype
 
+let data = "Shailendra Kumar Pandey";
+
+let data_1 = new String(data);
+
+console.log(data_1);    
+console.log(data);    
+
+// 1. charAt()
+
+console.log(data.charAt(2));        //   a
+
+console.log("Gayatri".charAt(4));   //   t
+
+// 2. toLowerCase()
+
+console.log(data.toLowerCase());  // shailendra kumar pandey
+
+console.log(data.toLocaleLowerCase());  // shailendra kumar pandey
+
+
+// 3. toUpperCase()
+
+console.log(data.toUpperCase());  // SHAILENDRA KUMAR PANDEY
+
+
