0593b76f49e727fc1fb964676f0f3ffcaa7223e4
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-08-28 21:49:22 +0530

    Practic Javascript

--- Javascript/Number Prototype Function/Practic Number Prototype Function.js
+++ Javascript/Number Prototype Function/Practic Number Prototype Function.js
@@ -1,2 +1,30 @@
 /* Number Prototype function */
 
+let data = new Number(42);
+
+console.log(data);  // [Number: 42]
+// 1. toString()
+
+console.log(data.toString());  // "42"          // return string
+
+
+// 2. valueOf()
+
+console.log(data.valueOf());  // 42             // return number
+
+// 3. toFixed()
+
+console.log(data.toFixed(2));  // "42.00"           // return string
+console.log(data.toFixed(0));  // "42"
+console.log(data.toFixed(5));  // "42.00000"
+
+// 4. toExponential()
+
+console.log(data.toExponential(2));  // "4.20e+1"           // return string
+console.log(data.toExponential(0));  // "4e+1"
+console.log(data.toExponential(5));  // "4.20000e+1"
+
+// 5. toPrecision()
+
+console.log(data.toPrecision(5));  // "42.00000"
+
