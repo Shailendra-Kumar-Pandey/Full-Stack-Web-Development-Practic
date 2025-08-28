0a5b246395cc679cc93264caa2155d3251811a1e
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-08-28 19:07:05 +0530

    Practic javascript

--- Javascript/Boolean Prototype function.js/practic prototype.js
+++ Javascript/Boolean Prototype function.js/practic prototype.js
@@ -11,3 +11,15 @@ console.log(data.toString());  // "true"
 
 console.log(data.valueOf());  // true
 
+// 3. hasOwnProperty()
+
+console.log(data.hasOwnProperty("valueOf"));  // false       // return boolean
+
+data.name = 1234;
+
+console.log(data.name);  // "John"
+
+console.log(data.hasOwnProperty("name"));  // true       // return boolean
+console.log(data.hasOwnProperty("toString"));  // false       // return boolean
+console.log(data.hasOwnProperty("toFixed"));  // false       // return boolean
+console.log(data.hasOwnProperty("valueOf"));  // true       // return boolean
