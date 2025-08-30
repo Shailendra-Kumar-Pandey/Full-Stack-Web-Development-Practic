4c8ade1344478000e9375a9aaee302284edfcafa
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-08-30 22:37:37 +0530

    Practic Javascript

--- Javascript/Array Prototype Function/Array function.js
+++ Javascript/Array Prototype Function/Array function.js
@@ -1,2 +1,23 @@
 /* Array Prototype method  */
 
+const data = [
+    {id: 1, name: "Shailendra", age: 27},
+    {id: 2, name: "Sandeep", age: 21},
+    {id: 3, name: "Jeetu", age: 25},
+    {id: 4, name: "Gayatri", age: 22},
+]
+
+// 01. forEach() method         - this function only itration  and not a return value
+
+data.forEach(function(ele, i, arr){
+    console.log(ele);
+    console.log(ele.name);
+    console.log(ele.age);
+    console.log(i);
+    console.log(arr);
+})
+
+data.forEach((ele)=>{
+    console.log(ele.id, ele.name, ele.age);
+})
+
