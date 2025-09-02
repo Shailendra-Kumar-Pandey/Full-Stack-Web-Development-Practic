d90d4dc0ba632f21615516322a889ca66aaa9992
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-09-02 23:41:21 +0530

    Practic Javascript

--- Javascript/Object/this function.js
+++ Javascript/Object/this function.js
@@ -1,1 +1,34 @@
 // Object
+
+const user = {
+    name:"Sumedh",
+    age:18,
+    address: {
+        pin:441100,
+        houseNo: 12,
+        landmark: "Near Bajaj Nagar",
+        city:"Nagpur"
+    },
+    hobby: ["Playing cricket", "Swimming"],
+    printMyName: function(){
+        console.log(this)
+    }
+}
+
+// console.log(user.address.houseNo)
+// console.log(user["address"]["houseNo"])
+
+// console.log(user.hobby)
+
+// user.hobby.forEach((ele)=>console.log(ele))
+
+// user.printMyName()
+
+
+
+
+function doSomething(...argument){
+    console.log(argument)
+}
+
+doSomething(10, 30)
