f43b059fad1a370c0fd77fc6428ba83a7d25b7a0
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-09-03 23:23:44 +0530

    Practic Javascript

--- Javascript/Date/Practic Date.js
+++ Javascript/Date/Practic Date.js
@@ -1,2 +1,15 @@
 //  This is a practice file for working with JavaScript Date objects
 
+let date = new Date();
+
+console.log(date);
+console.log(date.getFullYear());        //  2025
+console.log(date.getMonth());          //  0-11     
+console.log(date.getDate());          //  1-31      
+console.log(date.getHours());        //  0-23
+console.log(date.getMinutes());      //  0-59
+console.log(date.getSeconds());      //  0-59
+console.log(date.getMilliseconds()); //  0-999
+
+// let time = date.getTime();
+// console.log(time);
