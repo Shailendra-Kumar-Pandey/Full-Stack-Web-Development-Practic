bfb19d273e5040f098df0028eb06f5cf3c2d86b7
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-09-04 23:40:43 +0530

    Practic Javascript

--- Javascript/Math/practic Math Method.js
+++ Javascript/Math/practic Math Method.js
@@ -1,1 +1,15 @@
-// Math() Object
+// Math() Object Methods
+
+// 1. Common Math Methods Rounding Numbers
+
+// a. Math.round() - Rounds to nearest integer
+console.log(Math.round(4.7));  // 5
+console.log(Math.round(4.4));  // 4
+
+// b. Math.ceil() - Rounds up to next integer
+console.log(Math.ceil(4.1));   // 5
+console.log(Math.ceil(4.9));   // 5
+
+// c. Math.floor() - Rounds down to previous integer
+console.log(Math.floor(4.1));  // 4
+console.log(Math.floor(4.9));  // 4
