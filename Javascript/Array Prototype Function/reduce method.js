033f3a3d3a2a0626fd5edde1a7b283d53bc1d07d
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-09-04 22:42:41 +0530

    Practic Javascript

--- Javascript/Array Prototype Function/reduce method.js
+++ Javascript/Array Prototype Function/reduce method.js
@@ -3,10 +3,57 @@
 
 // reduce()        // Executes a reducer function (that you provide) on each element of the array, resulting in a single output value.
 
-const numbers = [1, 2, 3, 4, 5];
+/*
+Syntex:-
+    array.reduce(function(accumulator, currentValue) {
+   // logic
+}, initialValue);
 
-const sum = numbers.reduce(function(acc, curr) {        // acc = accumulator, curr = currentValue
-    return acc + curr;      // returns the sum of all elements
-}, 0);      //  initial value of accumulator is 0
+accumulator → पिछला result (संग्रह)
 
-console.log(sum);  // 15
+currentValue → अभी का element
+
+initialValue → शुरू में accumulator की value
+
+*/
+
+let numbers = [10, 20, 30, 40];
+
+let sum = numbers.reduce(function(total, num) {
+  return total + num;
+}, 0);
+
+console.log("Sum = " + sum);
+
+
+
+let number = [2, 3, 4];
+
+let product = number.reduce((total, num) => total * num, 1);
+
+console.log("Product = " + product);
+
+
+
+let number2 = [5, 17, 9, 21, 11];
+
+let max = number2.reduce((a, b) => (a > b ? a : b));
+
+console.log("Max = " + max);
+
+
+let cart = [
+  { item: "Book", price: 200 },
+  { item: "Pen", price: 50 },
+  { item: "Bag", price: 500 }
+];
+
+let totalPrice = cart.reduce((total, product) => total + product.price, 0);
+
+console.log("Total Price = " + totalPrice);         // Total Price = 750
+
+
+let number1 = [1, 2, 3, 4, 5];
+let sum1 = number1.reduce((a, b) => a + b, 0);
+console.log(sum1); // 15
+
---
+++ Javascript/Array Prototype Function/shortting method.js
@@ -0,0 +1,1 @@
+// short() method is used to shorten the length of an array by removing elements from the end.
