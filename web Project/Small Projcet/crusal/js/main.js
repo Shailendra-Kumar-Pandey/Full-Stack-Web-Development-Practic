1eb1bd4d50aac11d4f3e53bd22a93321a6d6acaa
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-09-10 04:18:09 +0530

    Practic Javascript

--- web Project/Small Projcet/crusal/js/main.js
+++ web Project/Small Projcet/crusal/js/main.js
@@ -1,16 +1,26 @@
-const rightBtn = document.getElementById('right-arrow');
-const leftBtn = document.getElementById('left-arrow');
-const imageContainer = document.getElementById('image-container');
+const rightBtn = document.getElementById("right-arrow");
+const leftBtn = document.getElementById("left-arrow");
+const imageContainer = document.getElementById("image-container");
 let currentIndex = 0;
 
+// console.log(imageContainer.children[0]);
+
 function rightSlide() {
-    const images = imageContainer.getElementsByTagName('img');
-    if (currentIndex < images.length - 1) {
-        currentIndex++;
-        const offset = -currentIndex * 300; // Assuming each image is 300px wide
-        imageContainer.style.transition = "all 0.3s"
+    if (currentIndex === 7) {
+        currentIndex = 0;
+        imageContainer.children[currentIndex];
+        imageContainer.children[currentIndex].src;
+        
+    } else {
+        
+        imageContainer.children[currentIndex += 1].src;
+        imageContainer.children[currentIndex += 1].style.transition = "all 10s ease-in-out";
+        for (let i = 0; i < imageContainer.children.length; i++) {
+            imageContainer.children[i].style.display = "none";
+        }
+        imageContainer.children[currentIndex].style.display = "block";
     }
-}
+ }
 function leftSlide() {
 
-}
+ }
