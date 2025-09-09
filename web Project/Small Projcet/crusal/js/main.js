192e3539476939fca3e67e23f812b9e844c35ba0
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-09-09 17:48:06 +0530

    Practic Javascript

--- web Project/Small Projcet/crusal/js/main.js
+++ web Project/Small Projcet/crusal/js/main.js
@@ -1,1 +1,16 @@
-const rightBtn = document.bo
+const rightBtn = document.getElementById('right-arrow');
+const leftBtn = document.getElementById('left-arrow');
+const imageContainer = document.getElementById('image-container');
+let currentIndex = 0;
+
+function rightSlide() {
+    const images = imageContainer.getElementsByTagName('img');
+    if (currentIndex < images.length - 1) {
+        currentIndex++;
+        const offset = -currentIndex * 300; // Assuming each image is 300px wide
+        imageContainer.style.transition = "all 0.3s"
+    }
+}
+function leftSlide() {
+
+}
