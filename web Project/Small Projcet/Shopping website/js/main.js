14dd48679ac35616e4d1823653ea00bd2176e0e4
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-09-10 23:59:40 +0530

    Practic Jaascript

---
+++ web Project/Small Projcet/Shopping website/js/main.js
@@ -0,0 +1,12 @@
+/*          <div class="card">
+                <div class="card-img"></div>
+                    <img src="https://d2lo0tepqt73yr.cloudfront.net/migProductImages/RC1200%20004-1.jpg" alt="Product 1">
+                </div>
+                <div class="card-content">
+                    <h2>Red Chief</h2>
+                    <p>₹ 3795</p>
+                    <p>Red Chief Genuine Leather Mid Ankle Lace Up Casual Shoes For Men |RC1200 004</p>
+                    <button>Add to Cart</button>
+                </div>
+            </div>
+*/
