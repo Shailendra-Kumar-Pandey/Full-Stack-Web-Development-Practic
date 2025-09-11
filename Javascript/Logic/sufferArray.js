d85b6d332e4295ef31d604d3ffc5b604125f1cd8
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-09-11 22:10:57 +0530

    Practic Javascript

---
+++ Javascript/Logic/sufferArray.js
@@ -0,0 +1,18 @@
+/*Write a Program to  Suffer array Logic  */ 
+
+const arr = [12, 31, 61, 71, 81, 99, 41, 53, 52, 74, 83, 94, 8, 5];
+
+let saffer = [];    // load num
+
+let index = [];     // load index
+
+let isComplite = true;
+
+while (isComplite) {
+    if (!index.includes()) {
+        
+    }
+    if () {
+        
+    }
+}
