288cdfa078518fff3932a19990bdb0335e350303
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-09-21 00:25:55 +0530

    Practic Javascript

--- Javascript/String Prototype Function/practic in string function.js
+++ Javascript/String Prototype Function/practic in string function.js
@@ -1,10 +1,12 @@
+//  Self Genrated Function only use String 
+
 let fullName = "Shailendra Shaligram Pandey"
 
 // 1. includes() method :- this function only return boolean value
 
 
 let result = fullName.includes('h')
-console.log(result)
+// console.log(result)
 
 
 function myIncludes(str, a ){
@@ -16,10 +18,10 @@ function myIncludes(str, a ){
             }
         }
     }else{
-        for(let i=0; i< str.length-1; i++){
-            for (let j = i; j < a.length-1; j++) {
-                
-                
+        for (let i = 0; i < str.length-a.length+1; i++) {
+            if(inputData === false){
+            // inputData = str.slice(i, i+a.length) === a;
+            inputData = mySlice(str, i , i+a.length) === a;
             }
         }
     }
@@ -27,3 +29,22 @@ function myIncludes(str, a ){
 }
 
 console.log(myIncludes(fullName, 'hai'))
+
+
+// 2. slice() method :- this function 
+
+// console.log(fullName.slice(0, 5))   
+//  result = fullName.slice(0, 5)
+ 
+//  console.log(result)
+
+function mySlice(string, startNumber, endNUmber ){
+    let receveData = "";
+    for (let i = startNumber; i < endNUmber; i++) {
+               receveData += string[i];
+    }
+    return receveData;
+}
+
+// console.log(mySlice(fullName, 4, 15))
+// console.log(mySlice('Shailendra', 0, 5))
