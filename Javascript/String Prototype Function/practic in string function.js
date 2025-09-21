693bb922583973a4c8ab02ddefe0f838aee19385
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-09-21 15:09:59 +0530

    Practic Javascript

--- Javascript/String Prototype Function/practic in string function.js
+++ Javascript/String Prototype Function/practic in string function.js
@@ -12,7 +12,7 @@ let result = fullName.includes('h')
 function myIncludes(str, a ){
     let inputData = false;
     if(a.length === 1){
-        for (let i = 0; i < str.length-1; i++) {
+        for (let i = 0; i < str.length; i++) {
             if(inputData === false){
             inputData = str[i] === a;
             }
@@ -28,7 +28,7 @@ function myIncludes(str, a ){
     return inputData
 }
 
-console.log(myIncludes(fullName, 'hai'))
+// console.log(myIncludes(fullName, 'hai'))
 
 
 // 2. slice() method :- this function 
@@ -48,3 +48,62 @@ function mySlice(string, startNumber, endNUmber ){
 
 // console.log(mySlice(fullName, 4, 15))
 // console.log(mySlice('Shailendra', 0, 5))
+
+
+
+// 3.charAt() method :- this function enter index no. return avilable string
+
+result = fullName.charAt(1)
+
+// console.log(result)
+
+function myCharAt(str, index){
+    let indexStr = ""
+    for (let i = 0; i < str.length; i++) {
+            if(str[index] === str[i]){
+                indexStr += str[i]
+                break;
+            }
+    }
+    return indexStr
+}
+
+// console.log(myCharAt(fullName, 0))
+
+
+// 4.toLowerCase() method :- this function will be return whole string are lower case convert
+
+result = fullName.toLowerCase();
+
+// console.log(result);
+
+function myToLowerCase(str) {
+    let lowerCase = ""
+    let holdLowerChar = " abcdefghijklmnopqrstuvwxyz";
+    let holdUpperChar = " ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let holdIndex;
+    for (let i = 0; i < str.length; i++) {
+        for(let j = 0; j < holdLowerChar.length; j++){
+            
+            if(str[i]===holdLowerChar[j]){
+                lowerCase += str[i]
+                break
+            }else{
+                for (let c = 0; c < holdUpperChar.length; c++) {
+                    if(str[i] === holdUpperChar[c]){
+                        holdIndex = c;
+                        break
+                    }
+                }
+                if (holdIndex === j) {
+                    lowerCase += holdLowerChar[j];
+                    holdIndex = null
+                    break
+                }
+            }
+        }
+    }    
+    return lowerCase;
+}
+
+console.log(myToLowerCase(fullName));
