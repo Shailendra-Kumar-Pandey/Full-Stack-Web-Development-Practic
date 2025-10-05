809946872cbaee943cf2f0d0b6ec69b741d30418
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-10-05 23:21:23 +0530

    Practic Javascript

--- web Project/Small Projcet/crusal/js/main.js
+++ web Project/Small Projcet/crusal/js/main.js
@@ -9,19 +9,44 @@ function rightSlide() {
     if (currentIndex === 7) {
         currentIndex = 0;
         // console.log(imageContainer.children[currentIndex]);
-        imageContainer.children[currentIndex].style.display = "block";
+        imageContainer.children[currentIndex].style.display = "flex";
 
     } else {
         // console.log(imageContainer.children[currentIndex]);
         imageContainer.children[currentIndex].style.display = "none";
-        imageContainer.children[currentIndex += 1].style.display = "block";
+        imageContainer.children[currentIndex += 1].style.display = "flex";
+
     }
  }
 // function leftSlide() {
 //     if (currentIndex === 0) {
+//         imageContainer.children[currentIndex].style.display = "none";
 //         currentIndex = 7;
-//         console.log(imageContainer.children[currentIndex-=1]);
+//         imageContainer.children[currentIndex].style.display = "block";
 //     } else {
-//          console.log(imageContainer.children[currentIndex -= 1]);
+//         imageContainer.children[currentIndex].style.display = "none";
+//         imageContainer.children[currentIndex -=1 ].style.display = "block";
 //     }
 //  }
+
+
+function leftSlide() {
+  // पहले current image को hide करो
+  imageContainer.children[currentIndex].style.display = "none";
+
+  // अगर currentIndex 0 है तो आखिरी image पर जाओ
+  if (currentIndex === 0) {
+    currentIndex = imageContainer.children.length - 1;
+  } else {
+    currentIndex = currentIndex - 1;
+  }
+
+  // अब नई image को दिखाओ
+  imageContainer.children[currentIndex].style.display = "block";
+}
+
+
+
+
+
+
