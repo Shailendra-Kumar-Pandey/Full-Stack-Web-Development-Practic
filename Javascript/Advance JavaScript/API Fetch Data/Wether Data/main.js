d1d621417d8162e4460e772de95dd0358cc67e24
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-10-12 15:58:30 +0530

    Practic Javascript

--- Javascript/Advance JavaScript/API Fetch Data/Wether Data/main.js
+++ Javascript/Advance JavaScript/API Fetch Data/Wether Data/main.js
@@ -27,16 +27,18 @@ function fetchWetherReport() {
 }
 
 function load(res) {
+    document.getElementById("heading").innerText = `${res.location.name}, ${res.location.region}, ${res.location.country} `
     document.getElementById("load").innerHTML = `
                                                   <div class="container">
                                                     <div class="inside-container">
                                             
-                                                        <div class="city">${res.location.name}</div>
-                                                        <div class="temp">Temperature ${res.current.temp_c}<sup>0</sup></div>
-                                                        <!--<div class="min-temp">Min-Temp</div>
-                                                         <div class="max-temp">Max-Temp</div> -->
+                                                        <div class="city">${res.location.name}
+                                                        <P>${res.location.localtime}</P></div>
+                                                        <div class="temp">Temperature ${res.current.temp_c} <sup>0</sup>C</div>
+                                                        <!--<div class="min-temp">Min-Temp</div> -->
+                                                        <div class="max-temp">Windchill ${res.current.windchill_c} <sup>0</sup>C</div> 
                                                         <div class="humidity">Humidity  ${res.current.humidity}%</div>
-                                                        <div class="pressure">Pressure</div>
+                                                        <div class="pressure">Pressure ${res.current.pressure_in}"</div>
                                                         <div class="wind-speed">Wind-Speed ${res.current.wind_kph}kmh</div>
                                                         <div class="state">${res.location.region}</div>
                                                         <div class="country">${res.location.country}</div>  
@@ -44,4 +46,17 @@ function load(res) {
                                                 </div>      
     
                                     `
+    if(res.current.condition.text === "Sunny" || res.current.condition.text ==="Clear"){
+        document.body.style.backgroundImage = "url('./Components/sunny.jpg')";
+    }else if(res.current.condition.text ==="Cloudy" || res.current.condition.text === "Partly cloudy"){
+        document.body.style.backgroundImage = "url('./Components/clouds.jpg')";
+    }else if(res.current.condition.text === "Light freezing rain" || res.current.condition.text === "Freezing drizzle" || res.current.condition.text === "Fog" || res.current.condition.text === "Freezing fog"){
+        document.body.style.backgroundImage = "url('./Components/freezing.jpg')";
+    }else if(res.current.condition.text === "Heavy rain" || res.current.condition.text === "Heavy rain at times" || res.current.condition.text === "Patchy rain possible"){
+        document.body.style.backgroundImage = "url('./Components/Thander.png')";
+    }else{
+        document.body.style.backgroundImage = "url('./Components/sunny.jpg')";
+
+    }
+    
 }
