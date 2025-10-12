af07a47e1cf971b34cb9f0cf65622b745c788b00
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-10-12 22:43:15 +0530

    Practic API

--- Javascript/Advance JavaScript/API Fetch Data/News/main.js
+++ Javascript/Advance JavaScript/API Fetch Data/News/main.js
@@ -1,1 +1,6 @@
 // News API and fetch data from it and display it in the browser
+
+const newsKey = "b73842219062446de118899d3359f40c"
+
+
+let url = `https://gnews.io/api/v4/top-headlines?category=sports&lang=en&country=in&max=10&apikey=${newsKey}`
