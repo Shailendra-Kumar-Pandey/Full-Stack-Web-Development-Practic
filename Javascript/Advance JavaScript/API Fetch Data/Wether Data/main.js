e3094bb43f47f550169d71616993cebd8710bb13
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-10-12 18:04:53 +0530

    PRactic JAvascript

--- Javascript/Advance JavaScript/API Fetch Data/Wether Data/main.js
+++ Javascript/Advance JavaScript/API Fetch Data/Wether Data/main.js
@@ -2,16 +2,10 @@
 
 const apiKey = "9093e8e77912426fb0c164841251110";
 
-// city.value
-
 let city = document.getElementById("city");
-
-// https://api.weatherapi.com/v1/current.json?key=9093e8e77912426fb0c164841251110&q=nagpur&aqi=yes
 
 function fetchWetherReport() {
     
-    // console.log(city.value)
-
     fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city.value}&aqi=yes`)
     .then((res)=>{
         return res.json();
@@ -21,22 +15,19 @@ function fetchWetherReport() {
         load(result)
     })
     .catch((err)=>{
-        console.log(err)
+        errorHandle(err);
     })
     
 }
-
 function load(res) {
     document.getElementById("heading").innerText = `${res.location.name}, ${res.location.region}, ${res.location.country} `
     document.getElementById("load").innerHTML = `
                                                   <div class="container">
                                                     <div class="inside-container">
-                                            
                                                         <div class="city">${res.location.name}
                                                         <P>${res.location.localtime}</P></div>
                                                         <div class="temp">Temperature ${res.current.temp_c} <sup>0</sup>C</div>
-                                                        <!--<div class="min-temp">Min-Temp</div> -->
-                                                        <div class="max-temp">Windchill ${res.current.windchill_c} <sup>0</sup>C</div> 
+                                                        <div class="wind-chill">Windchill ${res.current.windchill_c} <sup>0</sup>C</div> 
                                                         <div class="humidity">Humidity  ${res.current.humidity}%</div>
                                                         <div class="pressure">Pressure ${res.current.pressure_in}"</div>
                                                         <div class="wind-speed">Wind-Speed ${res.current.wind_kph}kmh</div>
@@ -56,7 +47,34 @@ function load(res) {
         document.body.style.backgroundImage = "url('./Components/Thander.png')";
     }else{
         document.body.style.backgroundImage = "url('./Components/sunny.jpg')";
-
     }
     
+
+    city.value = "";
+}
+
+let isDisplay = true;
+function errorHandle() {
+    if(isDisplay === true ){
+        document.getElementById("deleteModal").style.display = "flex";
+        setTimeout(()=>{
+            document.getElementById("deleteModal").style.display = "none";
+        }, 2000);
+        isDisplay = false
+        document.body.style.backgroundImage = "url('./Components/Thander.png')";
+        city.value = "";
+        document.getElementById("load").innerHTML = ""
+        document.getElementById("heading").innerText = ""
+
+    }else{
+        document.getElementById("deleteModal").style.display = "flex";
+        setTimeout(()=>{
+            document.getElementById("deleteModal").style.display = "none";
+        }, 2000);
+        isDisplay = true
+        document.body.style.backgroundImage = "url('./Components/Thander.png')";
+        city.value = ""
+        document.getElementById("load").innerHTML = ""
+        document.getElementById("heading").innerText = ""
+    }
 }
