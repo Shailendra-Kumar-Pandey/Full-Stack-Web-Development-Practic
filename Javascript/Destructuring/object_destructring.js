b05f82825837e76a47393f5415fef9c3675127fa
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-10-14 22:24:20 +0530

    Practic Javascript

--- Javascript/Destructuring/object_destructring.js
+++ Javascript/Destructuring/object_destructring.js
@@ -1,3 +1,19 @@
    // Object Destructuring
 
-   
+   const users = {
+        firstName : "Shailendra",
+        age : 30,
+        lastName : "Pandey",
+        mobileName : 9806638963
+   }
+
+   console.log(users);      // Print full object
+
+   // syntex object destructuring
+   const {firstName, lastName} = users;
+
+   console.log(firstName)   // Shailendra
+
+   console.log(lastName)    // Pandey
+
+//    console.log(age)      // error :- age is not define
