2f78b14389e5516f4d8c66ae94134739e95938d3
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-10-14 22:33:38 +0530

    Practic JAvascript

--- Javascript/Destructuring/array_destructuring.js
+++ Javascript/Destructuring/array_destructuring.js
@@ -1,2 +1,18 @@
 // Array destructuring
 
+const students = ["Shailendra", "Pandey", "Gayatri", "Sharma"]
+
+console.log(students)   // print full array
+
+//syntex:- array destructuring
+
+let [name1, name2] = students;
+
+// [index-number, index-number]
+
+
+console.log(name1)      // "Shailendra"
+
+console.log(name2)      // "Pandey"
+
+// console.log(name3)   // error:- name3 is not defined
