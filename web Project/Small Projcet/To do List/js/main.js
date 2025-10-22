cc8dda42f6f5bc125cf110553f788acc1af06502
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-10-22 21:44:23 +0530

    Practic Javascript

--- web Project/Small Projcet/To do List/js/main.js
+++ web Project/Small Projcet/To do List/js/main.js
@@ -1,1 +1,21 @@
-//
+
+    // const todoNote = [];
+    
+    let todoList = document.getElementById("todo-list")
+    let todoNote = `<div class="container">
+                        <button onclick="closeNote()">X</button>
+                        <div class="inner-container">
+                            <textarea id="todo-text" onkeyup="addNote(this.value)" placeholder="Type a new task..."></textarea>
+                        </div>
+                    </div>`
+    todoList.innerHTML += todoNote;
+
+    function addNewItem(){
+        todoList.innerHTML += todoNote;
+    }
+
+    function addNote(){
+
+        console.log(document.getElementById("todo-text").value)
+
+    }
