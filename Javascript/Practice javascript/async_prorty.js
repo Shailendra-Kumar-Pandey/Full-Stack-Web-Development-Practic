1671d66b065f2d26322d63010591c6df547cd471
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-10-24 02:34:30 +0530

    Practic javascript

---
+++ Javascript/Practice javascript/async_prorty.js
@@ -0,0 +1,21 @@
+// 
+
+console.log(1)
+
+setTimeout(()=>{
+    console.log(2)
+}, 0)
+
+console.log(3)
+
+setTimeout(()=>{
+    console.log(4)
+}, 0)
+
+console.log(5)
+
+setTimeout(()=>{
+    console.log(6)
+}, 0)
+
+console.log(7)
