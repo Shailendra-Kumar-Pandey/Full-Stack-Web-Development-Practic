4561111d42052d33b2c19cdea139f89d6a4918d7
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-10-24 03:03:45 +0530

    Practic Javscript

--- Javascript/Loops/for_of_loop.js
+++ Javascript/Loops/for_of_loop.js
@@ -1,6 +1,12 @@
 let arr = [1, 2, 3, 4, 5];
 
-for (const value of arr) {
+//  this loop are all array value itrabale
+
+for (let value of arr) {
+    console.log(value)
+}
+
+for (let value of arr) {
     value += 10;
     console.log(value)
 }
