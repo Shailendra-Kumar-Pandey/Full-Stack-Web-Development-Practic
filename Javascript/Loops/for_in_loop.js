0cdeba136c7d8cf15abd5d1265a128003dd3be25
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-10-24 03:14:25 +0530

    Practic Javscript

--- Javascript/Loops/for_in_loop.js
+++ Javascript/Loops/for_in_loop.js
@@ -1,5 +1,14 @@
 const obj = { a: 1, b: 2, c: 3 };
 
 for (const i in obj) {
+    console.log(i);
   console.log(obj[i]);
+}
+
+
+const arr = [10, 20, 30];
+
+for (const index in arr) {
+    console.log(index);
+  console.log(arr[index]);
 }
