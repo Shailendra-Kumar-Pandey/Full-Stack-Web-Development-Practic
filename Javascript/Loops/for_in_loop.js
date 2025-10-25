e3740ed01756cd1279e12e471b5609aab61a75e7
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-10-25 21:57:57 +0530

    Add comment

--- Javascript/Loops/for_in_loop.js
+++ Javascript/Loops/for_in_loop.js
@@ -1,3 +1,4 @@
+// for in loop iterates over the enumerable properties of an object.
 const obj = { a: 1, b: 2, c: 3 };
 
 for (const i in obj) {
@@ -5,6 +6,8 @@ for (const i in obj) {
   console.log(obj[i]);
 }
 
+
+// Example with an array
 
 const arr = [10, 20, 30];
 
