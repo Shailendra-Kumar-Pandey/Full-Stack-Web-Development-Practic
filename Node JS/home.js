ffde47a35f39078035d01913d28fc13b9ba1a977
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-11-03 21:04:22 +0530

    Practic Node JS

---
+++ Node JS/home.js
@@ -0,0 +1,14 @@
+const calculator = require("./introdutionModules.js")       // import module :- using require method
+
+const add = calculator.addition(20, 10)
+
+const sub = calculator.subtraction(20, 10)
+
+const multi = calculator.multiplication(20, 10)
+
+const divid = calculator.divition(20, 10)
+
+console.log(add)
+console.log(sub)
+console.log(multi)
+console.log(divid)
