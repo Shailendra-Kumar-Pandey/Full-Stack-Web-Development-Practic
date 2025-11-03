bfa89af0c44da9d45cbbae2be2a8fe78eabdfafa
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-11-03 20:54:33 +0530

    Practic Javascript

--- Node JS/introdutionModules.js
+++ Node JS/introdutionModules.js
@@ -1,9 +1,27 @@
 // What is a Module :- Collection of function is called module.
 // What is Pakeage :- Collection of modues is called Pakeage Manager.
 
-// 
+// Create a Calculater function
+
+function addition(num1, num2){
+    return num1 + num2;
+}
+
+
+function subtraction(num1, num2){
+    return num1 - num2;
+}
+
+
+function multiplication(num1, num2){
+    return num1 * num2;
+}
+
+
+function divition(num1, num2){
+    return num1 / num2;
+}
 
 
 
-
-// module.exports = 
+module.exports = {addition, subtraction, multiplication, divition} 
