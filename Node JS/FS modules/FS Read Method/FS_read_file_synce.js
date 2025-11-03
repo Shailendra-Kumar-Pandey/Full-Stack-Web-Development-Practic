f756eae456a2b695d350a6dae20968b7598e2c20
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-11-03 21:25:48 +0530

    Practic Node JS

---
+++ Node JS/FS modules/FS Read Method/FS_read_file_synce.js
@@ -0,0 +1,15 @@
+const fs = require("fs")
+
+//  file system module is a enbuild Javascript modules
+// readFileSync function :- this function is a synchronus task, this function is a execute code line by line
+
+console.log("1")
+let result = fs.readFileSync("./hello.txt", {encoding : "utf-8"});
+
+console.log("2", result)
+
+console.log("3")
+
+
+
+
