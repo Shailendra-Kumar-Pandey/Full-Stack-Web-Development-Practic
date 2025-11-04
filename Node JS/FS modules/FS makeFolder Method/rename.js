8ec98c071aa2af0193c44d925421808b2fe4decb
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-11-05 01:02:03 +0530

    Practic Node JS

--- Node JS/FS modules/FS makeFolder Method/rename.js
+++ Node JS/FS modules/FS makeFolder Method/rename.js
@@ -1,3 +1,10 @@
 const fileSystem = require('fs')
 
-fileSystem.renameSync('./New Folder', './Jeetu Bhai')
+// fileSystem.renameSync('./New Folder', './Jeetu Bhai')        // THis function is Synchronous Function
+
+// This is Asynchronous Method using by Call-Backs
+fileSystem.rename('./Jeetu Bhai', './Data', (err)=>{
+    if(!err){
+        console.log("Rename file Successfully Complited!")
+    }
+})
