e1dcc49b8e52012e6088605011bafc469508eedb
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-11-04 23:03:12 +0530

    Practic Node JS

--- Node JS/FS modules/FS Write Method/write_file-asynce.js
+++ Node JS/FS modules/FS Write Method/write_file-asynce.js
@@ -1,3 +1,10 @@
 // fs module:-
 const fileSystem = require('fs')
 
+fileSystem.writeFile("./demo.txt", "Hey, What is your Name?", (err)=>{
+    if(!err){
+        console.log("Create file Successfully!");
+    }
+})
+
+// writefile function :- This function is a asynchronus task using Create File and add data Also.
--- Node JS/FS modules/FS Write Method/write_file_synce.js
+++ Node JS/FS modules/FS Write Method/write_file_synce.js
@@ -4,4 +4,4 @@ const fileSystem = require('fs')
 
 fileSystem.writeFileSync("./demo.txt", "Hello, My name is Shailendra \nMobile No. 9806638963")
 
-// writeFileSync Function :- This function is a synchronus function
+// writeFileSync Function :- This function is a synchronus function , This function is Create and Write.
