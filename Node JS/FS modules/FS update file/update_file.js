0c0ae624898961eb3e21a730075aa07e42bf05b4
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-11-05 00:21:07 +0530

    Practic Node JS

--- Node JS/FS modules/FS update file/update_file.js
+++ Node JS/FS modules/FS update file/update_file.js
@@ -1,6 +1,6 @@
 // FS Module :-
 const fileSystem = require('fs')
-
+/*  // This is using aSynchronous function using and update file
 fileSystem.readFile("./demo.txt",{encoding : "utf-8"}, (err, data)=>{
     if(!err){
         let result = data;
@@ -11,3 +11,10 @@ fileSystem.readFile("./demo.txt",{encoding : "utf-8"}, (err, data)=>{
         })
     }
 })
+*/
+
+//  Here use to Synchronous Function using and update file
+
+let result = fileSystem.readFileSync("./demo.txt",{encoding:"utf-8"})
+
+fileSystem.writeFileSync('./demo.txt',result.replace('This is', 'It is'))
