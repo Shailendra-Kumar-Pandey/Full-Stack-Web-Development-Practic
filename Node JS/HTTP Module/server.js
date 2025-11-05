053fb28a4c8004de356c96b8a0400c30c37fff75
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-11-05 22:50:21 +0530

    Practic Node JS

--- Node JS/HTTP Module/server.js
+++ Node JS/HTTP Module/server.js
@@ -1,25 +1,30 @@
 // HTTP Module :- This is very Powerfull Module using in Application 
 
-const http = require('http')
+const http = require('http')        
 
-const server = http.createServer((req, res)=>{
-    console.log(req.method)
+const server = http.createServer((req, res)=>{      // Create Server
+    console.log(req.method)        
     console.log(req.url)
 
-    if(req.url === '/getusers' && req.method === 'GET'){
+    if(req.url === '/getusers' && req.method === 'GET'){        // API to get Users
+        const data = {              // Example data
+            name : "Shailendra Kumar Pandey",
+            email : 'shailendrakr2010@gmail.com',
+            mobile : 9806638963
+        }
 
-        res.writeHead(200, {"content-type": "plain/text"});
+        res.writeHead(200, {"content-type": "application/json"});       // Response Header
 
-        res.end("Hey My Name is Shailendra Kumar Pandey!")
+        res.end(JSON.stringify(data))       // Response Body
 
     }else{
-        res.writeHead(404, {"content-type": "plain/text"});
+        res.writeHead(404, {"content-type": "application/json"});       // Response Header for Page Not Found
 
-        res.end("Page Not Found!")
+        res.end("Page Not Found!")     // Response Body 
 
     }
 })
 
-server.listen(8000, ()=>{
-    console.log("SERVER is Running....")
+server.listen(8000, ()=>{           // Server Listen on Port 8000
+    console.log("SERVER is Running....")        // Server Running Message
 })
