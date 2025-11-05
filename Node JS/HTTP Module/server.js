11dc17e0cf638635fb376569938c5e9c72b3ec10
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-11-05 20:52:34 +0530

    Practic Node JS

--- Node JS/HTTP Module/server.js
+++ Node JS/HTTP Module/server.js
@@ -1,14 +1,12 @@
-// HTTP Module :-
+// HTTP Module :- This is very Powerfull Module using in Application 
 
 const http = require('http')
 
 const server = http.createServer((req, res)=>{
-
-    
-
+    res.writeHead(200, {"content-type": "plain/text"});
+    res.end("Hey My Name is Shailendra Kumar Pandey!")
 })
 
 server.listen(8000, ()=>{
-    console.log("Server is Running");
+    console.log("SERVER is Running....")
 })
-
