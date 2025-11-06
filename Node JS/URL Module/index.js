ca82bae8d8b05f3fa546f50d8a8e2ca4948b1211
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-11-06 22:40:31 +0530

    Practic Node JS

---
+++ Node JS/URL Module/index.js
@@ -0,0 +1,8 @@
+// URL Module:- The URL module provides utilities for URL resolution and parsing.
+
+const http = require('http');
+const url = require('url');
+
+http.createServer((req, res)=>{
+
+})
