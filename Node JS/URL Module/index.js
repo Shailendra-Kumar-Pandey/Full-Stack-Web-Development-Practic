5d96f399eb7aeb6891be2397d6bd344a7fded9e5
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-11-06 23:37:49 +0530

    Practic Node JS

--- Node JS/URL Module/index.js
+++ Node JS/URL Module/index.js
@@ -3,6 +3,28 @@
 const http = require('http');
 const url = require('url');
 
-http.createServer((req, res)=>{
 
+const server = http.createServer((req, res)=>{
+
+    if(req.url === "/user" && req.method === "GET"){
+        const data = {
+            name :"shailendra Kumar Pandey",
+            mobile : 9806638963,
+            email : "shailendrakr2010@gmail.com"
+        }
+
+        res.writeHead(200, {"content-type": "application/json"});
+        res.end(data)
+    }else{
+
+        res.writeHead(404, {"content-type" : "application/json"});
+        res.end(JSON.stringify({
+            message:"Not Found!"
+        }))
+    }
+
+})
+
+server.listen(2000, ()=>{
+    console.log("Server is Running...")
 })
