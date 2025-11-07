152bd161c160081bfeb500292841ec0b3e237025
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-11-07 18:12:45 +0530

    Practic  Node JS

--- Node JS/Project/index.js
+++ Node JS/Project/index.js
@@ -5,15 +5,17 @@ const server = http.createServer((req, res)=>{
 
     let data = '';
     if(req.url === "/getallusers" && req.method === "GET"){
+     
+        
         
     }
-    req.on("data", (chunk)=>{
-        console.log(chunk)
-    })
+    // req.on("data", (chunk)=>{
+    //     console.log(chunk)
+    // })
 
-    req.on("end", ()=>{
+    // req.on("end", ()=>{
 
-    })
+    // })
 
     res.writeHead(200, {'content-type':'application/json'});
 
