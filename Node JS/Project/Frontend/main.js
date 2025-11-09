252a5dd30dfcf297e7feec972b7b990ffdb23edc
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-11-09 20:16:14 +0530

    Practice Node JS

--- Node JS/Project/Frontend/main.js
+++ Node JS/Project/Frontend/main.js
@@ -1,2 +1,17 @@
 // Frontend Part of This JS file
 
+const baseURL = "http://localhost:2000";
+const URLgetAllStudents = "/getAllStudents";
+
+function getAllStudents(){
+    fetch(`${baseURL}${URLgetAllStudents}`, {method : 'GET'})
+    .then((res)=>{
+        return res.json;
+    })
+    .then((response)=>{
+        console.log(response);
+    })
+    .catch((err)=>{
+        console.log(err, `${URLgetAllStudents}`);
+    })
+}
