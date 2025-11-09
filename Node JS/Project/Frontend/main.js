4675516690a73387f6e2f5544577e8e741b4d95e
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-11-09 21:07:02 +0530

    Practice Node JS

--- Node JS/Project/Frontend/main.js
+++ Node JS/Project/Frontend/main.js
@@ -3,6 +3,44 @@
 const baseURL = "http://localhost:2000";
 const URLgetAllStudents = "/getAllStudents";
 
+// Open Close Modal
+let modal = false;
+function openClose(modalType) {
+    if(modal === false){
+        document.getElementById(modalType).style.display = "flex";
+        modal = true;
+    }else{
+        document.getElementById(modalType).style.display = "none";
+        modal = false;
+    }
+}
+                /*
+                    <tr>
+                            <td>1</td>
+                            <td>Shailendera Kumar Pandey</td>
+                            <td>shailendera@gmail.com</td>
+                            <td>MBA</td>
+                            <td>9806638963</td>
+                            <td>
+                                <button class="action-btn edit-btn" onclick="openClose('editModal')">Edit</button>
+                                <button class="action-btn delete-btn" onclick="openClose('deleteModal')">Delete</button>
+                            </td>
+                    </tr>
+                */
+
+    const studentData = []
+
+function displayAllData(){
+
+    let row = '';
+
+
+}
+
+
+
+
+// Get All Student API function
 function getAllStudents(){
     fetch(`${baseURL}${URLgetAllStudents}`, {method : 'GET'})
     .then((res)=>{
