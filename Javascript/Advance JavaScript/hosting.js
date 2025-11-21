b325b219e561b41e1812ff710e3c97a81d6ca271
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-11-21 23:46:16 +0530

    Practice Javascript

---
+++ Javascript/Advance JavaScript/hosting.js
@@ -0,0 +1,6 @@
+// Hosting in JavaScript
+// what is Hosting?
+// Hoisting is a JavaScript mechanism where variables and function declarations are moved to the top of their containing scope during the compilation phase before the code is executed. This means that you can use variables and functions before they are declared in the code.
+// Hoisting applies to both variable declarations (using var, let, and const) and function declarations. However, the behavior differs based on how the variables are declared.
+
+// Example 1: Hoisting with var
