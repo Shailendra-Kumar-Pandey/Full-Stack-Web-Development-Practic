05019ebb77feb1d61aec58551cf2f12ea8bd943a
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-12-04 00:20:56 +0530

    Practice Javascript

--- Javascript/Destructuring/array_destructuring.js
+++ Javascript/Destructuring/array_destructuring.js
@@ -16,3 +16,12 @@ console.log(name1)      // "Shailendra"
 console.log(name2)      // "Pandey"
 
 // console.log(name3)   // error:- name3 is not defined
+
+
+// what is destructuring
+// destructuring is a way to unpack values from arrays or properties from objects into distinct variables
+
+// Example of destructuring with default values
+let [name3 = "Default Name", name4 = "Another Default"] = students;
+console.log(name3)      // "Gayatri"
+console.log(name4)      // "Sharma"
