5df4490aec02e811eec9e72604d96a93cc3fc433
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-12-15 16:30:59 +0530

    Practice JAvascript

--- Javascript/Loops/1primeNumber.js
+++ Javascript/Loops/1primeNumber.js
@@ -14,3 +14,20 @@ function isPrimeORnotPrime(num){
 }
 
 isPrimeORnotPrime(2)
+
+let count = 0;
+let num = 0
+for(let i = 2; count < 10000; i++ ){
+    let isPrime = true;
+    for(let j=2; j < i; j++){
+        if(i%j == 0){
+            isPrime = false;
+            break;
+        }
+    }
+    if(isPrime){
+        console.log(i);
+        count++
+        console.log(num++)
+    }
+}
