1620d627d31d01112def8352293cb7ff98cf0064
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-12-18 23:37:20 +0530

    Practice MongoDB

--- mongoDB/01_create_database.js
+++
@@ -1,8 +0,0 @@
-use ("college")
-
-db.students.inserOne({
-    name: "John Doe",
-    age: 21,
-    major: "Computer Science",
-    enrolled: true
-})
---
+++ mongoDB/01_create_database.mongodb.js
@@ -0,0 +1,14 @@
+
+
+// How to Create Data Base and Create Database
+
+const database = "College";         // use this type store the variable in a dataBase
+
+const collection = "Students"       // use this type create a new collection
+
+use(database)       // 
+
+db.createCollection(collection)
+
+db.createCollection('teachers')
+
