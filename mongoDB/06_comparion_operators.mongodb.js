668edbde6361bfef089d02d39bcd26e3fc56c4f9
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-12-19 01:59:34 +0530

    Practice MongoDB

--- mongoDB/06_comparion_operators.mongodb.js
+++ mongoDB/06_comparion_operators.mongodb.js
@@ -20,13 +20,26 @@ use('College')
 
 // $gt: Matches values that are greater than a specified value.
 
-    db
+    db.Students.find({age : {$gt : 20}})
 
 // $gte: Matches values that are greater than or equal to a specified value.
 
+    db.Students.find({age : {$gte: 20}})
+
 // $in: Matches any of the values specified in an array.
 
+    db.Students.find({age: {$in: [19, 22]}})
 
 // $nin: Matches none of the values specified in an array.
+
+    db.Students.find({age: {$nin: [19, 20, 21, 22]}})
+
+// $all: Matches arrays that contain all elements specified in the query.
+
+    // db.Students.find({age: {$all: [19]}})
+
+// $size: Matches arrays with the specified number of elements.
+
+    // db.Students.find({age: {$size: 10}})
 
     
