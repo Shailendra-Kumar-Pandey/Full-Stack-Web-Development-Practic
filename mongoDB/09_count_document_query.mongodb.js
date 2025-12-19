76b0b55a0fa7aee31e571ca34c3872e34703bf12
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-12-20 01:05:23 +0530

    Practice Javascript

--- mongoDB/09_count_document_query.mongodb.js
+++ mongoDB/09_count_document_query.mongodb.js
@@ -1,5 +1,7 @@
 use('College');
 
+// countDocuments():- this method is used to count the number of documents in a collection that match a specified query. If no query is provided, it counts all documents in the collection.
+
 // Count all documents in the Students collection
     
     db.Students.countDocuments()
@@ -7,3 +9,11 @@ use('College');
 // Count documents where age is greater than 20
     
     db.Students.countDocuments({ age: { $gt: 20 } })
+
+// Count documents where major is 'Computer Science'
+    
+    db.Students.countDocuments({ major: 'Computer Science' })
+
+// Count documents where enrolled is true
+    
+    db.Students.countDocuments({ age: 20 })
