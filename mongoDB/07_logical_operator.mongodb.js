737f7d20c12ce929413dd07ef4851b6758dab1fb
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-12-20 00:37:14 +0530

    Practice Javascript

--- mongoDB/07_logical_operator.mongodb.js
+++ mongoDB/07_logical_operator.mongodb.js
@@ -4,15 +4,28 @@ use('College')
 
 
 // $and: Joins query clauses with a logical AND returns all documents that match the conditions of both clauses.
+// this opretor 
 /* Examples:
      db.Students.find({ $and: [ { age: { $gt: 18 } }, { age: { $lt: 25 } } ] })
 */
 
+     db.Students.find({$and : [{age:20}]})
+
+     db.Students.find({$and : [{age:20} ,{course:'BCA'}]})
+     
+     // db.Students.find()
+
 // $or: Joins query clauses with a logical OR returns all documents that match the conditions of either clause.
+
+     db.Students.find({$or : [{age:20}, {course:'BCA'}]})        
 
 
 // $nor: Joins query clauses with a logical NOR returns all documents that fail to match both clauses.
 
+     db.Students.find({$nor : [{age:20}, {course:'BCA'}]})        
+
 
 // $not: Inverts the effect of a query expression and returns documents that do not match the query expression.
+
+     db.Students.find({$not : [{age:20}, {course:'BCA'}]})
 
