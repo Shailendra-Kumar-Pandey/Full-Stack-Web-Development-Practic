70ca1f2130c89ba825ccb14bdea543608b9ed83e
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-12-20 00:57:19 +0530

    Practice Javascript

--- mongoDB/08_delete_database.mongodb.js
+++ mongoDB/08_delete_database.mongodb.js
@@ -1,2 +1,27 @@
 use('College');
 
+// Deleting the document
+
+// deleteOne():- This method is used to delete a single document that matches the specified filter.
+
+    db.Students.deleteOne({ name: 'Neha Singh'})
+
+    db.Students.find()
+
+// deleteMany():- This method is used to delete multiple documents that match the specified filter.
+
+    db.Students.deleteMany({ age: { $gt: 22 }})
+
+    db.Students.find()
+
+
+// drop():- This method is used to delete an entire collection from the database.
+
+    // db.Students.drop()
+
+// dropDatabase():- This method is used to delete the entire database.
+
+    // db.dropDatabase()
+    // use('College');
+    // show dbs 
+    // 
