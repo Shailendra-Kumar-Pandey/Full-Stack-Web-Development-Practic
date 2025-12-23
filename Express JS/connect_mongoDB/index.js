51058e004489a52c01774c982dcaa3480493ae9c
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-12-24 00:42:19 +0530

    Practice express

---
+++ Express JS/connect_mongoDB/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+// const mongoos = require('mongoos');
+
+const app = express();
+
+
+
+
+
+
+app.listen(5050, ()=>{
+    console.log("Server is Running...");
+})
