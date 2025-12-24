c88d621163b770c6af5a38a393a7f18cccf9c01e
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-12-25 00:18:25 +0530

    Practice mongoDB

--- Express JS/connect_mongoDB/index.js
+++ Express JS/connect_mongoDB/index.js
@@ -24,11 +24,23 @@ const studentSchema = new mongoose.Schema(
     {strict:false}
 )
 
+
+//Build in middleware
+app.use(express.json())
+
 // Create a MongoDb new Collection 
 const Students = mongoose.model('student', studentSchema)
 
+// POST API 
+app.post('/addStudent', async (req,res)=>{
+    
+    const payload = await Students.create(req.body)
 
-
+    res.send({
+        massage: "Student Add Create ",
+        result: payload
+    })
+})
 
 
 
