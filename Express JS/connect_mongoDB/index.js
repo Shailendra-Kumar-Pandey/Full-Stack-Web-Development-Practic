c25d61166d787247811f7ae0696fee4dfc531914
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-12-24 23:35:42 +0530

    Practice mongoDB

--- Express JS/connect_mongoDB/index.js
+++ Express JS/connect_mongoDB/index.js
@@ -1,7 +1,31 @@
 const express = require('express');
-// const mongoos = require('mongoos');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// mongoose.connect("mongodb://127.0.0.1:27017/Student-management-systam" )
+
+// Connect to mongoDB Server 
+const connectDB = async () => {
+  try {
+        await mongoose.connect("mongodb://127.0.0.1:27017/Student-management-system");
+        console.log("MongoDB Connected Successfully");
+  } catch (error) {
+        console.error(error);
+        console.log("MongoDB Connection Failed");
+  }
+};
+
+connectDB()
+
+
+const studentSchema = new mongoose.Schema(
+    {},
+    {strict:false}
+)
+
+// Create a MongoDb new Collection 
+const Students = mongoose.model('student', studentSchema)
 
 
 
