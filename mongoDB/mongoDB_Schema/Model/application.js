53e852197958b6374a99a96cd1b2d5bde77d0530
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-12-27 20:37:13 +0530

    Practice express and mongoose

--- mongoDB/mongoDB_Schema/Model/application.js
+++ mongoDB/mongoDB_Schema/Model/application.js
@@ -1,7 +1,24 @@
 import mongoose from "mongoose";
 
 const applicationSchema = new mongoose.Schema(
-    {},
+    {
+        jobName :{
+            type : mongoose.Schema.Types.ObjectId,
+            ref : 'job',
+            required : true
+        },
+        companyName:{
+            type : mongoose.Schema.Types.ObjectId,
+            ref : 'company',
+            required : true
+        },
+        status: {
+          type : String,
+          enum : ['applied', 'shortlisted', 'rejected'],
+          default : 'applied',
+          required : true  
+        }
+    },
     {
         timestamps: true
     }
