6749d642234a06ed966c7c5dabdb8fe3020f7f74
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-12-27 20:56:38 +0530

    Practice express and mongoose

--- mongoDB/mongoDB_Schema/Model/Job.js
+++ mongoDB/mongoDB_Schema/Model/Job.js
@@ -2,7 +2,26 @@ import mongoose from 'mongoose';
 
 const jobSchema = new mongoose.Schema(
     {
-
+        jobName :{
+            type : String,
+            required : true,
+            trim : true
+        },
+        companyName:{
+            type : mongoose.Schema.Types.ObjectId,
+            ref : 'company',
+            required: true
+        },
+        description : {
+            type : String,
+            required : true,
+            trim : true
+        },
+        HR_name : {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : 'user',
+            required: true
+        }
     },
     {
         timestamps : true,
