6400c22b7bd3bc281261eb8db8a4b5c638fa9888
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-12-28 22:37:32 +0530

    Practice database Schema

---
+++ mongoDB/School-Management-System/Model/Staff.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const staffSchema = new mongoose.Schema(
+  {
+    staff_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    father_name: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    department: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    address: {
+        type: String,
+        required: true,
+        trim: true
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Staff", staffSchema);
--- mongoDB/School-Management-System/Model/Student.js
+++ mongoDB/School-Management-System/Model/Student.js
@@ -2,7 +2,31 @@ import mongoose from 'mongoose';
 
 const studentSchema = new mongoose.Schema(
     {
-
+        student_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        },
+        class: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        father_name: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        address: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        imgURL:{
+            type: String,
+            required: true,
+            trim: true
+        },                                                    
     },
     {
         timestamps: true,
