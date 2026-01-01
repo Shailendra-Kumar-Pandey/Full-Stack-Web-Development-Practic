ae96643f889d88337a799a5d2ed387f0b6e0c195
Shailendra Pandey <shailendrakr2010@gmail.com>
2026-01-02 01:05:51 +0530

    Practice React JS

--- React JS/my-app/src/App.js
+++ React JS/my-app/src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
 import Header from './Components/Header';
+import Main from './Components/Main'
+
+
 
 function App() {
   return (
@@ -8,6 +11,10 @@ function App() {
       <hr />
 
       <p>lorenm ipsum dolor sit amet consectetur adipisicing elit. Dolore iure quidem, commodi suscipit, perferendis, expedita provident cumque unde laboriosam maiores animi quo fuga atque nisi debitis recusandae ad quisquam itaque?</p>
+
+      <h5>What is your Name? <span style={{color: 'darkblue'}}>Shailendra Kumar Pandey</span></h5>
+
+      <Main />
 
       <hr />
 
