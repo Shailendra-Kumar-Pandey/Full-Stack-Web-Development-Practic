9a0e0bbc4e395350e0e12f4f8f5b40505dc522d8
Shailendra Pandey <shailendrakr2010@gmail.com>
2026-01-02 00:38:27 +0530

    PRactice React JS

---
+++ React JS/my-app/src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+    return (
+    <div>
+        <h1>Welcome to My App</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore iure quidem, commodi suscipit, perferendis, expedita provident cumque unde laboriosam maiores animi quo fuga atque nisi debitis recusandae ad quisquam itaque?</p>
+    </div>
+    );
+}
+
+export default Header;
