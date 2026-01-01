94f319aa9284cb7697b0c8d65dd64f0929a164bc
Shailendra Pandey <shailendrakr2010@gmail.com>
2026-01-02 00:39:06 +0530

    PRactice React JS

--- React JS/my-app/src/App.js
+++ React JS/my-app/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <hr />
+
+      <p>lorenm ipsum dolor sit amet consectetur adipisicing elit. Dolore iure quidem, commodi suscipit, perferendis, expedita provident cumque unde laboriosam maiores animi quo fuga atque nisi debitis recusandae ad quisquam itaque?</p>
+
+      <hr />
+
+      <Header />
+    </>
   );
 }
 
