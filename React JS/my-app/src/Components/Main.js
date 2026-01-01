f6945fce208b229b9e0ac4811c69fe07d3318bd3
Shailendra Pandey <shailendrakr2010@gmail.com>
2026-01-02 00:57:11 +0530

    Practice React Js

---
+++ React JS/my-app/src/Components/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const main = ()=>{
+    return (
+        <header>
+            <h1>My is my Firest Web Site</h1>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita nemo repudiandae esse dicta doloremque debitis dolores eveniet, incidunt in corporis iste, consequuntur, quaerat suscipit. Quia ab pariatur ipsam architecto est.</p>
+
+        </header>
+    );
+}
