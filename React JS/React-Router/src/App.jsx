2577eda088597f1e10a7ebc6852e615bb20ec013
Shailendra Pandey <shailendrakr2010@gmail.com>
2026-01-18 00:52:40 +0530

    Practice React Router

--- React JS/React-Router/src/App.jsx
+++ React JS/React-Router/src/App.jsx
@@ -1,11 +1,20 @@
-import './App.css'
-
+import { BrowserRouter, Route, Link, Routes} from 'react-router';
 
 function App() {
 
   return (
     <>
-      
+      <h1>React Router in My Project</h1>
+      <BrowserRouter>
+        <Link to='/'>Home</Link>
+        <Link to='/About'>About</Link>
+        <Link to='Contect'>Contect</Link>
+          <Routes>
+            <Route path='/' element={ <h1>Home </h1> } />
+            <Route path='/About' element={ <h1> About </h1> } />
+            <Route path='/Contect' element={ <h1>Contect </h1> } />
+          </Routes>
+      </BrowserRouter>
     </>
   )
 }
