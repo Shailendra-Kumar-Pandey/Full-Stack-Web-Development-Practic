29c7bf6bfc46ef7c569974d5b22f45523093120f
Shailendra Pandey <shailendrakr2010@gmail.com>
2026-01-26 23:59:22 +0530

    PRactice react

---
+++ React JS/useActionState Hook/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import LogIN from './Component/Login'
+
+function App() {
+
+  return (
+    <>
+      <h1>Using a useActionState Hook</h1>
+      <LogIN />
+    </>
+  )
+}
+
+export default App
---
+++ React JS/useActionState Hook/src/Component/Login.jsx
@@ -0,0 +1,11 @@
+
+
+const LogIN = ()=>{
+    return(
+        <>
+            <h1>Welcome the React Hook</h1>
+        </>
+    )
+}
+
+export default LogIN
