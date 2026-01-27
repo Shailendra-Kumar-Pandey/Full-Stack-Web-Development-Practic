ad37af6f41160e276d303701dadf84a1dc77a7eb
Shailendra Pandey <shailendrakr2010@gmail.com>
2026-01-28 00:50:53 +0530

    Practice express JS

---
+++ Express JS/demo/model/Landload.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const landloadSchema = mongoose.Schema(
+    {
+        userID:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'user',
+            required: true
+        },
+        address:{
+            type: String,
+            required: true,
+            trim : true,
+            minlength: 5
+        },
+        
+        rent:{
+            type: Number,
+            required: true,
+        },
+        status:{
+            type:String,
+            enum:['APPROVED', 'PENDING', 'REJECT', 'BLOCK'],
+            default: 'PENDING'
+        }
+    },
+    {
+        timestamps: true
+    }
+)
---
+++ Express JS/demo/model/User.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+
+
+const userSchema = mongoose.Schema(
+    {
+        fullName :{
+            type : String,
+            required: true,
+            trim : true,
+            minlength: 5
+        },
+        email:{
+            type: String,
+            required: true,
+            trim : true,
+            unique: true
+        },
+        mobile:{
+            type: String,
+            required: true,
+            trim : true,
+        },
+        Password:{
+            type: String,
+            required: true,
+            trim : true,
+            minlength: 6
+        },
+        role:{
+            type: String,
+            enum:['USER', 'ADMIN', 'LANDLOAD'],
+            default: 'USER',
+            required: true
+        },
+        isActive:{
+            type:Boolean,
+            default:'true'
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+
+
+export default mongoose.model('user', userSchema);
