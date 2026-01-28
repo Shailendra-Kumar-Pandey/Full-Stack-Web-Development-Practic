fc35cee09a3895dcc105392a6f58369e9ee69f47
Shailendra Pandey <shailendrakr2010@gmail.com>
2026-01-28 22:42:10 +0530

    Practice Express

--- Express JS/demo/app.js
+++ Express JS/demo/app.js
@@ -1,7 +1,17 @@
 import express from 'express';
+import routerAuth from './routers/auth.js';
+import routerAdmin from './routers/admin.js';
+import routerLandload from './routers/landload.js';
 
 const app = express()
 
+// Build In midleware
+app.use(express.json());
+
+//Router in API
+app.use('/admin', routerAdmin)      // http://127.0.0.1:9000/admin
+app.use('/landload', routerLandload )       // http://127.0.0.1:9000/landload
+app.use('/auth', routerAuth)        // http://127.0.0.1:9000/auth
 
 
 export default app;
--- Express JS/demo/routers/auth.js
+++ Express JS/demo/routers/auth.js
@@ -1,8 +1,9 @@
 import express from 'express';
+import { ragistrationUser } from '../controller/authController.js';
 
 const router = express.Router()
 
-
+router.post('/ragistration', ragistrationUser)
 
 
 export default router;
