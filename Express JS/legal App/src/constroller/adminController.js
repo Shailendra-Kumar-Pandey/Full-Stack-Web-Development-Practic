858d3861818ee44271d83d151fe0bb81530dfc24
Shailendra Pandey <shailendrakr2010@gmail.com>
2026-02-16 01:41:13 +0530

    API Call

--- Express JS/legal App/src/constroller/adminController.js
+++ Express JS/legal App/src/constroller/adminController.js
@@ -13,3 +13,45 @@ export const fetchLawyers = async(req, res)=>{
         return res.status(500).json({massage : `${error}, Server Error...`})
     }
 }
+
+
+
+export const lawyerStatus = async(req, res)=>{
+
+    try {
+
+        let {status, adminRemark} = req?.body;
+        
+        let profileId = req?.params?.profileID
+        
+        if(!profileId){
+            return res.status(404).json({massage: "Please Provide lawyer Profile ID..."})
+        }
+
+        if(!status || !adminRemark){
+            return res.status(404).json({massage: "All Field Required..."})
+        }
+
+        let existLawyerProfile = await lawyerModel.findById(profileId);
+
+        if(!existLawyerProfile){
+            return res.status(404).json({massage:"Please Enter Vailid Lawyer Profile ID"});
+        }
+    
+        existLawyerProfile.status = status;
+
+        existLawyerProfile.adminRemark = adminRemark;
+
+        status === "APPROVED" ? existLawyerProfile.approvedAt = new Date() : null;
+
+        await existLawyerProfile.save()         // mongoose function {save()}
+        
+        return res.status(201).json({
+            massage: "Lawyer Status Change...",
+            result : existLawyerProfile
+        })
+
+    } catch (error) {
+        return res.status(500).json({massage : `${error}, Server Error...`})
+    }
+}
--- Express JS/legal App/src/routers/admin.js
+++ Express JS/legal App/src/routers/admin.js
@@ -1,9 +1,13 @@
 import express from 'express';
-import { fetchLawyers } from '../constroller/adminController.js';
+import { fetchLawyers, lawyerStatus } from '../constroller/adminController.js';
 import authantication from '../middleware/authantication.js';
 const router = express.Router()
 
 
 router.get('/getAllLawyers', authantication, fetchLawyers);
+
+
+router.put('/changeLawyerStatus/:profileID', authantication, lawyerStatus);
+
 
 export default router;
