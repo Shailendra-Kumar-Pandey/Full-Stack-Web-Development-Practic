e00131c2c8831fc08c8ce2324bac797ce3dcb043
Shailendra Pandey <shailendrakr2010@gmail.com>
2026-02-19 00:55:20 +0530

    Practice useEffect hooks

--- React JS/useEffect/src/App.jsx
+++ React JS/useEffect/src/App.jsx
@@ -1,26 +1,29 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Users from "./Users";
 
 function App(){
+    let [data, setData] = useState([])
     useEffect(()=>{
         fetch("https://jsonplaceholder.typicode.com/users", {
             method:"GET"
         })
         .then((res)=>res.json())
         .then((response)=>{
-            console.log(response)
+            setData(response)
         })
         .catch((err)=>{
             console.log(err)
         })
     },[])
+
+    // console.log(data)
     return(
         <>
             <div className="title">
                 <h1>User Data</h1>
             </div>
 
-            <Users/>
+            <Users userData={data}/>
 
         </>
     );
--- React JS/useEffect/src/Users.jsx
+++ React JS/useEffect/src/Users.jsx
@@ -1,10 +1,36 @@
-function Users(){
-    return(
-        <>
-        
-        </>
-    );
-}
+function Users({ userData }) {
+//   console.log(userData);
 
+  return (
+    <>
+      <div className="container">
+        <table>
+          <thead className="tablehadder">
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Mobile No</th>
+              <th>Web-Side</th>
+            </tr>
+          </thead>
+          <tbody>
+            {userData.map((e, i) => {
+              return (
+                <tr key={i}>
+                  <td>{e.id}</td>
+                  <td>{e.name}</td>
+                  <td>{e.email}</td>
+                  <td>{e.phone}</td>
+                  <td>{e.website}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+}
 
 export default Users;
