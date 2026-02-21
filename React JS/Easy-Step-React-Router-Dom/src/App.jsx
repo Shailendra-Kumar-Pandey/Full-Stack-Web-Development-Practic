9f3ac725dc61395e915bd623d9858a7b1901cc3c
Shailendra Pandey <shailendrakr2010@gmail.com>
2026-02-22 00:00:21 +0530

    add component

--- React JS/Easy-Step-React-Router-Dom/src/App.jsx
+++ React JS/Easy-Step-React-Router-Dom/src/App.jsx
@@ -1,8 +1,19 @@
+import React from "react";
+import Home from './Component/Home';
+import About from './Component/About';
+import Product from './Component/Product';
+import Contact from './Component/Contact';
 
 function App(){
   return(
     <>
         <h1>React Router Dom</h1>
+        <div>
+          <Home/>
+          <About/>
+          <Product/>
+          <Contact/>
+        </div>
     </>
   );
 }
---
+++ React JS/Easy-Step-React-Router-Dom/src/Component/Product.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Product() {
+
+    
+  return (
+    <div>
+      <button>Product 1</button>
+      <button>Product 2</button>
+      <button>Product 3</button>
+      <button>Product 4</button>
+    </div>
+  )
+}
+
+export default Product
