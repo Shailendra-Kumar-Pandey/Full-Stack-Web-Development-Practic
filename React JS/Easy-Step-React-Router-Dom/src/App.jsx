e09ab56c8e01d202d40170c1c2f6afee9d33294d
Shailendra Pandey <shailendrakr2010@gmail.com>
2026-02-22 22:18:13 +0530

    Practice Routing

--- React JS/Easy-Step-React-Router-Dom/src/App.jsx
+++ React JS/Easy-Step-React-Router-Dom/src/App.jsx
@@ -3,16 +3,24 @@ import Home from './Component/Home';
 import About from './Component/About';
 import Product from './Component/Product';
 import Contact from './Component/Contact';
+import Header from "./Component/Header";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App(){
   return(
     <>
-        <h1>React Router Dom</h1>
+        
         <div>
-          <Home/>
-          <About/>
-          <Product/>
-          <Contact/>
+          <BrowserRouter>
+          <Header/>
+              <Routes>
+                  <Route path="/" element={<Home/>}/>
+                  <Route path="/About" element={<About/>}/>
+                  <Route path="/Product" element={<Product/>}/>
+                  <Route path="/Contact" element={<Contact/>}/>
+              </Routes>
+          </BrowserRouter>
+          
         </div>
     </>
   );
--- React JS/Easy-Step-React-Router-Dom/src/Component/Header.jsx
+++ React JS/Easy-Step-React-Router-Dom/src/Component/Header.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <div>
-        
-    </div>
+    <>
+        <div className="main">
+          <h2>LOGO</h2>
+          <ul>
+            <li>
+              <Link to= "/"> Home</Link>
+            </li>
+            <li>
+              <Link to="/About"> About</Link>
+            </li>
+            <li>
+              <Link to="/Product"> Product</Link>
+            </li>
+            <li>
+              <Link to="/Contact"> Contact</Link>
+            </li>          
+          </ul>
+        </div>
+    </>
   )
 }
 
-export default Header
+export default Header;
