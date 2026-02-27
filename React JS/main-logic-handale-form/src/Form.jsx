376d608d7e63c355567dc4c3c6c02a4471d45511
Shailendra Pandey <shailendrakr2010@gmail.com>
2026-02-27 23:38:11 +0530

    Practice JSX

--- React JS/main-logic-handale-form/src/Form.jsx
+++ React JS/main-logic-handale-form/src/Form.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import {useNavigate } from 'react-router-dom'
 
 function Form() {
+
+  const navigator = useNavigate();
+
+  function handleNavigator(){
+    navigator('/')
+  }
   return (
+   <>
+   <h1 className="text-2xl font-bold text-center p-2">Data Form</h1>
     <div className="flex justify-center items-center flex-col gap-2">
       <label htmlFor="name" > Full Name
         <input type="text" id="name" placeholder="Enter Your FullName" onKeyUp={() => {}} />
@@ -12,8 +21,9 @@ function Form() {
       <label htmlFor="password"> Password
         <input type="text" id="password" placeholder="Enter Your Password" onKeyUp={() => {}} />
       </label>
-      <button>Submit</button>
+      <button onClick={handleNavigator}>Submit</button>
     </div>
+   </>
   );
 }
 
