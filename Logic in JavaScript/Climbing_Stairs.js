240678d8793c6de3b442f281deacb1fe8a11c62e
Shailendra Pandey <shailendrakr2010@gmail.com>
2026-03-07 21:44:04 +0530

    JS logic Practice

--- Logic in JavaScript/Climbing_Stairs.js
+++ Logic in JavaScript/Climbing_Stairs.js
@@ -1,9 +1,24 @@
-// What is the Climbing Stairs in how to solve the problem , write a program 
-// example : -   
+// What is the Climbing Stairs in how to solve the problem , write a program
+// example : -
 //  input = 1       output = 1   // 1
 //  input = 2       output = 2  // 1+1; 2
 //  input = 3       output = 3  // 1+1+1; 2+1; 1+2
 //  input = 4       output = 5  // 1+1+1+1; 2+1+1; 1+2+1; 1+1+2; 2+2
 //  input = 5       output = 8  // 1+1+1+1+1; 2+1+2; 2+2+1; 1+2+2; 2+1+1+1; 1+2+1+1; 1+1+2+1; 1+1+1+2
 
+function climbingStairs(numberOfStairs) {
+    if (numberOfStairs <= 2) {
+        return numberOfStairs;
+    }
 
+    let a = 1;
+    let b = 2;
+    for (let i = 3; i <= numberOfStairs; i++) {
+        let c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
+}
+
+console.log(climbingStairs(20));
---
+++ Logic in JavaScript/Fibonacci_Number.js
@@ -0,0 +1,8 @@
+/*   Write a program create a function any number Enter Fibonacci Number serige
+    for Example:- 
+
+    input = 2    output = 1;
+    input = 4    output = 2;
+*/ 
+
+
