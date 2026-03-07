bde9f806a94c24603d454b610e596dff5a2347e3
Shailendra Pandey <shailendrakr2010@gmail.com>
2026-03-07 21:31:22 +0530

    JS LOGIC

---
+++ Logic in JavaScript/Climbing_Stairs.js
@@ -0,0 +1,9 @@
+// What is the Climbing Stairs in how to solve the problem , write a program 
+// example : -   
+//  input = 1       output = 1   // 1
+//  input = 2       output = 2  // 1+1; 2
+//  input = 3       output = 3  // 1+1+1; 2+1; 1+2
+//  input = 4       output = 5  // 1+1+1+1; 2+1+1; 1+2+1; 1+1+2; 2+2
+//  input = 5       output = 8  // 1+1+1+1+1; 2+1+2; 2+2+1; 1+2+2; 2+1+1+1; 1+2+1+1; 1+1+2+1; 1+1+1+2
+
+
