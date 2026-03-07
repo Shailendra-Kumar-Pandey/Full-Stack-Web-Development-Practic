10bdd4646a978cd34484f8a0f6fa6115a04722a1
Shailendra Pandey <shailendrakr2010@gmail.com>
2026-03-07 21:58:00 +0530

    JS logic Practice

--- Logic in JavaScript/Fibonacci_Number.js
+++ Logic in JavaScript/Fibonacci_Number.js
@@ -5,4 +5,25 @@
     input = 4    output = 2;
 */ 
 
+function fibbonacciFind(numberOfValue){
 
+    let a = 0, b = 1;
+
+    if(numberOfValue === 1){
+        return a
+    }else if(numberOfValue === 2){
+        return b
+    }else{
+        for(let i = 2; i < numberOfValue; i++){
+            let c = a + b;
+            a = b;
+            b = c
+            // console.log(c)
+        }
+    }
+
+    return b;
+}
+
+
+console.log(fibbonacciFind(10))
