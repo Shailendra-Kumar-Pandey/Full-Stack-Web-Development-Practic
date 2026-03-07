1120892f950810fe013c040507896d8bdec5f9a7
Shailendra Pandey <shailendrakr2010@gmail.com>
2026-03-07 23:43:48 +0530

    JS logic Practice

---
+++ Logic in JavaScript/Longest_Common_Prefix.js
@@ -0,0 +1,29 @@
+/*
+    Problem Statement
+Write a function to find the longest common prefix string amongst an array of strings. If there is no common prefix, return an empty string.
+
+Input Format
+Array of strings
+
+Output Format
+String representing longest common prefix
+
+Constraints
+
+Sample Test Cases
+Test Case 1
+Input:
+["flower","flow","flight"]
+Expected Output:
+"fl"
+Explanation:
+Common prefix is 'fl'
+
+Test Case 2
+Input:
+["dog","racecar","car"]
+Expected Output:
+""
+Explanation:
+No common prefix
+*/ 
--- Logic in JavaScript/Remove_Duplicates_from_Sorted_Array.js
+++ Logic in JavaScript/Remove_Duplicates_from_Sorted_Array.js
@@ -1,1 +1,15 @@
 // Write a Program to Sort the dublicate Number find and sord and display in output
+
+function solution(arr) {
+    let a = []
+    for(let i=0; i<arr.length; i++){
+       if(!a.includes(arr[i])){
+         a.push(arr[i])
+     }
+   }
+  return a;
+}
+
+
+let data = [0,0,0,1,1,2,3,3,3,4,4,4]
+console.log(solution(data));
