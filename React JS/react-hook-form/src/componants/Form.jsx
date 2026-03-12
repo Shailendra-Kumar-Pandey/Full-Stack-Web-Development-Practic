4c047da64f5f5d2aa3e1bdb32c341b0324ffe6b0
Shailendra Pandey <shailendrakr2010@gmail.com>
2026-03-13 01:25:54 +0530

    practice react hook form

--- React JS/react-hook-form/src/componants/Form.jsx
+++ React JS/react-hook-form/src/componants/Form.jsx
@@ -3,9 +3,9 @@ import { useForm } from "react-hook-form";
 
 
 function Form({onNavigate}) {
-     const { register, handleSubmit } = useForm();
+     const { register, handleSubmit, formState: { errors } } = useForm();
 
-  function onSubmit(data) {
+  function outputData(data) {
     console.log(data);
     onNavigate()
   }
@@ -15,40 +15,31 @@ function Form({onNavigate}) {
         <h1 className="font-bold p-4">React Hook Form</h1>
       </div>
       <div className="w-full flex justify-center items-center">
-        <form className="bg-sky-400 w-full " onSubmit={handleSubmit(onSubmit)}>
-          <label htmlFor="id">ID</label>
-          <input 
-            type="id"
-            name="id" 
-            id="id" 
-            placeholder="Enter Vailid ID"
-            {...register('id')}
-          />
-          <label htmlFor="name">Full Name</label>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            placeholder="Enter Full Name"
-            {...register('name')}
+        <form className="bg-sky-400 w-full " onSubmit={handleSubmit(outputData)}>
+          <label >ID</label>
+          <input placeholder="Enter Vailid ID" {...register('id', {required:true, maxLength:10})} />
 
+          {errors.id && <span style={{color:'red'}}>This field is required</span>}
+
+          <label >Full Name</label>
+          <input placeholder="Enter Full Name" {...register('name',{required:true})}
           />
+          {errors.name && <span style={{color:'red'}}>This field is required</span>}
+
           <label htmlFor="email">Email</label>
           <input
-            type="email"
-            name="email"
-            id="email"
             placeholder="Enter Email Address"
-            {...register('email')}
+            {...register('email',{required:true})}
           />
+          {errors.email && <span style={{color:'red'}}>This field is required</span>}
+
           <label htmlFor="password">Password</label>
           <input
-            type="password"
-            name="password"
-            id="password"
             placeholder="Enter Your Password"
-            {...register('password')}
+            {...register('password', {required:true})}
           />
+          {errors.password && <span style={{color:'red'}}>This field is required</span>}
+
           <button type="submit">submit</button>
         </form>
       </div>
